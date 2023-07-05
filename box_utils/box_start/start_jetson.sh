5892c1522925398a7aa7ccec58aaffaffc730dd3
Benjamin <benjamin.krummenacher@outlook.com>
2023-07-05 14:48:50 +0200

    wip

--- box_utils/box_start/start_jetson.sh
+++ box_utils/box_start/start_jetson.sh
@@ -8,7 +8,12 @@ NUC_IP="192.168.1.56"
 
 # start roscore
 source /opt/ros/noetic/setup.bash
-roscore
+roscore&
+sleep 3
 
 echo $JETSON_IP
 rostopic list
+
+# Wait for NUC to be ready
+until ping -c1 $NUC_IP >/dev/null 2>&1; do :; done
+echo "Found NUC"
