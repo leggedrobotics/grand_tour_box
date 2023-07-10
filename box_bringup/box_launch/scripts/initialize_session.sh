59eb9638bd93cf82f35c5c3d4a23fc39d5313e46
schmirob <schmirob@student.ethz.ch>
2023-07-10 22:01:59 +0200

    First rviz plugin

--- box_bringup/box_launch/scripts/initialize_session.sh
+++ box_bringup/box_launch/scripts/initialize_session.sh
@@ -3,5 +3,10 @@
 # Kill previous process
 rosrun box_launch cleanup_session.sh
 
+set -g mode-mouse on
+set -g mouse-select-pane on
+set -g mouse-resize-pane on
+set -g mouse-select-window on
+
 # run cleanup script on exit
 tmux set-hook -g 'session-closed' 'run "rosrun box_launch cleanup_session.sh"'
