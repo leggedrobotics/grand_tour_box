4c3369ae615c6c4e6dd1629eefaa80464ede3729
Benjamin Krummenacher <bkrumme@student.ethz.ch>
2023-07-26 14:14:49 +0200

    add nuc config

---
+++ box_configuration/nuc/nuc_configure.sh
@@ -0,0 +1,16 @@
+cp /home/rsl/catkin_ws/src/grand_tour_box/box_configuration/nuc/01-network-manager-all.yaml /etc/netplan/01-network-manager-all.yaml
+sudo netplan generate
+sudo netplan apply
+
+
+cp /home/rsl/catkin_ws/src/grand_tour_box/box_configuration/nuc/autostart_tmux.service /etc/systemd/system/autostart_tmux.service
+cp /home/rsl/catkin_ws/src/grand_tour_box/box_configuration/nuc/phc2sys_NIC.service /lib/systemd/system/phc2sys_NIC.service
+cp /home/rsl/catkin_ws/src/grand_tour_box/box_configuration/nuc/phc2sys_system.service /lib/systemd/system/phc2sys_system.service
+cp /home/rsl/catkin_ws/src/grand_tour_box/box_configuration/nuc/ptp4l_enp45s0.service /lib/systemd/system/ptp4l_enp45s0.service
+cp /home/rsl/catkin_ws/src/grand_tour_box/box_configuration/nuc/ptp4l_enp46s0.service /lib/systemd/system/ptp4l_enp46s0.service
+sudo systemctl daemon-reload
+sudo enable autostart_tmux
+sudo enable phc2sys_NIC
+sudo enable phc2sys_system
+sudo enable ptp4l_enp45s0
+sudo enable ptp4l_enp46s0
