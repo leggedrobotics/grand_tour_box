bd929af17318c876b7df546cc103e6c4d9a76d21
Benjamin Krummenacher <bkrumme@student.ethz.ch>
2023-08-04 14:13:37 +0200

    refactor, topic rate is also published when  it is 0

--- box_utils/box_health/src/publish_sensor_freq.py
+++ box_utils/box_health/src/publish_sensor_freq.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import rospy
+import rospy, rostopic
 from sensor_msgs.msg import Image, CameraInfo
 from std_msgs.msg import Float32
 import time
@@ -8,33 +8,20 @@ import yaml
 import socket
 
 class FrequencyPublisher:
-    def __init__(self, topic_name):
-        self.topic_name = topic_name
-        self.window_duration = 1.0  # Time window for calculating instantaneous frequency (in seconds)
+    def __init__(self, topic):
+        self.topic = topic
+        self.rt = rostopic.ROSTopicHz(-1)
+        self.sub = rospy.Subscriber(self.topic, rospy.AnyMsg, self.rt.callback_hz, callback_args=self.topic) 
+        self.publisher = rospy.Publisher(self.topic + "/rate", Float32, queue_size=10)
+        rospy.sleep(1)   
 
-        self.message_counter = 0
-        self.window_start_time = time.time()
-        self.frequency = 0.0
-
-        rospy.Subscriber(self.topic_name, rospy.AnyMsg, self.callback)
-        self.publisher = rospy.Publisher(self.topic_name + "/rate", Float32, queue_size=10)
-
-    def callback(self, data):
-        self.message_counter += 1
-        current_time = time.time()
-
-        # Check if the time window has passed
-        if current_time - self.window_start_time >= self.window_duration:
-            elapsed_time = current_time - self.window_start_time
-            self.frequency = float(self.message_counter) / elapsed_time
-
-            # Publish frequency as a Float32
-            self.publisher.publish(self.frequency)
-
-            # Reset counters and start time for the next window
-            self.message_counter = 0
-            self.window_start_time = current_time
-
+    def publish_hz(self):
+        hz_status = self.rt.get_hz(self.topic)
+        if hz_status:
+            self.publisher.publish(hz_status[0])
+        else:
+            self.publisher.publish(0.0)
+        
 
 if __name__ == '__main__':
     rospy.init_node('frequency_publisher')
@@ -43,12 +30,16 @@ if __name__ == '__main__':
     hostname = socket.gethostname() 
 
     # Read camera topics from ROS parameter
-    camera_topics = rospy.get_param(hostname, [])
-    print(camera_topics)
+    topics = rospy.get_param(hostname, [])
 
     publishers = []
-    for topic in camera_topics:
+    for topic in topics:
         publisher = FrequencyPublisher(topic)
         publishers.append(publisher)
+
+    while not rospy.is_shutdown():
+        time.sleep(1)
+        for pub in publishers:
+            pub.publish_hz()
 
     rospy.spin()
