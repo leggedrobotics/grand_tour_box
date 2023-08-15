052fc22a337068a1ea2c50427588fa2c24c479fb
Benjamin <benjamin.krummenacher@outlook.com>
2023-08-15 09:54:27 +0200

    added replay mode

--- box_utils/box_recording/bin/copy_mission_data_from_robot.sh
+++ box_utils/box_recording/bin/copy_mission_data_from_robot.sh
@@ -11,25 +11,25 @@ robot_name=$2
 local_path_to_save_data=$3
 local_path_to_save_data="${local_path_to_save_data//\~/$HOME}"
 
-today=$(date +"%Y_%m_%d")
-local_path_to_save_bags=${local_path_to_save_data}/${robot_name}_mission_${today}
-local_path_to_save_logs=${local_path_to_save_bags}/${robot_name}_logs
+# today=$(date +"%Y_%m_%d")
+# local_path_to_save_bags=${local_path_to_save_data}/${robot_name}_mission_${today}
+# local_path_to_save_logs=${local_path_to_save_bags}/${robot_name}_logs
 
-if [ ! -d "${local_path_to_save_bags}" ]; then
-  mkdir -p "${local_path_to_save_bags}"
-fi
+# if [ ! -d "${local_path_to_save_bags}" ]; then
+#   mkdir -p "${local_path_to_save_bags}"
+# fi
 
-if [ ! -d "${local_path_to_save_logs}" ]; then
-  mkdir -p "${local_path_to_save_logs}"
-fi
+# if [ ! -d "${local_path_to_save_logs}" ]; then
+#  mkdir -p "${local_path_to_save_logs}"
+# fi
 
 if [ "$robot_name" == "jetson" ] || [ "$robot_name" == "Jetson" ]; then
   # Jetson: mission rosbags, background rosbags, tmux log and git log
-  rsync --progress ${user}@192.168.2.51:~/git/grand_tour_box/box_utils/box_recording/data/* ${local_path_to_save_bags}
+  rsync --progress ${user}@192.168.2.51:~/git/grand_tour_box/box_utils/box_recording/data/* ${local_path_to_save_data}
   # rsync --progress ${user}@192.168.2.51:~/mission_log/* ${local_path_to_save_logs}
   
 elif [ "$robot_name" == "nuc" ] || [ "$robot_name" == "Nuc" ] || [ "$robot_name" == "NUC" ]; then
   # NUC: mission rosbags, background rosbags, tmux log and git log
-  rsync --progress ${user}@192.168.2.56:~/git/grand_tour_box/box_utils/box_recording/data/* ${local_path_to_save_bags}
+  rsync --progress ${user}@192.168.2.56:~/git/grand_tour_box/box_utils/box_recording/data/* ${local_path_to_save_data}
   # rsync --progress ${user}@192.168.2.56:~/mission_log/* ${local_path_to_save_logs}
 fi
