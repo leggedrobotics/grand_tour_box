cbfe075d35e7f1d2bc34be98b8ad39bad525549b
Benjamin <benjamin.krummenacher@outlook.com>
2023-08-18 16:30:11 +0200

    fix bugs

--- box_utils/box_health/src/merge_health_status.py
+++ box_utils/box_health/src/merge_health_status.py
@@ -3,8 +3,7 @@
 import socket
 import rospy
 
-from box_health.msg import healthStatus
-from publish_local_health_status import default_healthstatus
+from box_health.msg import healthStatus, healthStatus_jetson, healthStatus_nuc
 
 # TODO(beni) add mutex?
 class BoxStatusMerger:
@@ -14,64 +13,46 @@ class BoxStatusMerger:
 
         rospy.init_node('health_status_merger')
 
-        # todo(beni) remove
-        self.external_hostnames = ['jetson', 'nuc']
-        self.subscribers = []
-
-        for external_host in self.external_hostnames:
-            topic = self.namespace + 'health_status/' + external_host
-            sub = rospy.Subscriber(topic, healthStatus, self.callback)
-            self.subscribers.append(sub)
-        
+        self.subscriber_jetson = rospy.Subscriber('/' + "health_status/jetson", healthStatus_jetson, self.callback, "jetson")
+        self.subscriber_nuc = rospy.Subscriber('/' + "health_status/nuc", healthStatus_nuc, self.callback, "nuc")
         # names have to exactly match healthStatus.msg
         # TODO(beni): read directly from file
-        self.message_fields = [
-            "offset_mgbe0_systemclock",
-            "offset_mgbe0_mgbe1",
-            "status_mgbe0_ptp4l",
-            "status_mgbe1_ptp4l",
-            "offset_enp45s0_systemclock",
-            "offset_enp45s0_enp46s0",
-            "offset_mgbe0_enp45s0",
-            "status_enp46s0_ptp4l",
-            "gt_box_alphasense_driver_node_cam3_hz",
-            "gt_box_alphasense_driver_node_cam4_hz",
-            "gt_box_alphasense_driver_node_cam5_hz",
-            "gt_box_hesai_pandar_hz",
-            "gt_box_livox_lidar_hz",
-            "gt_box_livox_imu_hz",
-            "gt_box_alphasense_driver_node_imu_hz",
-            "gt_box_rover_piksi_position_receiver_0_ros_pos_enu_hz",
-            "gt_box_adis16475_hz",
-            "gt_box_usb_cam_image_hz",
-            "gps_num_sat",
-            "gps_rtk_mode_fix",
-            "gps_fix_mode",
-            "gps_utc_time_ready",
-        ]
-
+        self.message_fields = {
+            "jetson" : [
+                "offset_mgbe0_systemclock",
+                "offset_mgbe0_mgbe1",
+                "status_mgbe0_ptp4l",
+                "status_mgbe1_ptp4l",
+                "gt_box_alphasense_driver_node_cam3_hz",
+                "gt_box_alphasense_driver_node_cam4_hz",
+                "gt_box_alphasense_driver_node_cam5_hz",
+                "gt_box_hesai_pandar_hz",
+                "gt_box_livox_lidar_hz",
+                "gt_box_livox_imu_hz",
+                "gt_box_alphasense_driver_node_imu_hz",
+                "gt_box_rover_piksi_position_receiver_0_ros_pos_enu_hz",
+                "gps_num_sat",
+                "gps_rtk_mode_fix",
+                "gps_fix_mode",
+                "gps_utc_time_ready"
+            ],
+            "nuc" : [
+                "offset_enp45s0_systemclock",
+                "offset_enp45s0_enp46s0",
+                "offset_mgbe0_enp45s0",
+                "status_enp46s0_ptp4l",
+                "gt_box_adis16475_hz",
+                "gt_box_image_raw_hz",
+            ]
+        }
         self.health_status_publisher = rospy.Publisher(self.namespace + 'health_status/merged' , healthStatus, queue_size=10)
         self.rate = rospy.Rate(1)
-        
-        self.complete_health_msg = default_healthstatus()
+        self.complete_health_msg = healthStatus()
 
-    def return_if_set(self, value):
-        if isinstance(value, int) or isinstance(value, float) :
-            if value != -1:
-                return value
-            else:
-                return -1       
-        elif isinstance(value, str):
-            if value != "empty":
-                return str(value)
-            else:
-                return "empty"
-        else:
-            rospy.logerr("[BoxStatusMerger] Value " + str(value) + " has unknown type:" + str(type(value)))
-
-    def callback(self, partial_health_data):
-        for field in self.message_fields:
-            setattr(self.complete_health_msg, field, self.return_if_set(getattr(partial_health_data, field)))
+    def callback(self, partial_health_data, sender):
+        rospy.loginfo("[BoxStatusMerger]" + sender)
+        for field in self.message_fields[sender]:
+            setattr(self.complete_health_msg, field, getattr(partial_health_data, field))
     
     def publish_complete_health_status(self):
         while not rospy.is_shutdown():
