98c9b63a74d422695d6585ede4c482d762a1ddf2
Benjamin <benjamin.krummenacher@outlook.com>
2023-08-28 11:05:09 +0200

    fix log error

--- box_utils/box_health/src/publish_local_health_status.py
+++ box_utils/box_health/src/publish_local_health_status.py
@@ -121,7 +121,6 @@ class BoxStatus:
             health_msg.offset_enp45s0_systemclock = self.check_clock_offset(self.read_clock_status("phc2sys_system.service"))
         else:
             rospy.logerr("[BoxStatus] This hostname is unknown: " + self.hostname)
-
         return health_msg
     
     def get_frequency_if_available(self, topic):
@@ -154,7 +153,7 @@ class BoxStatus:
         process = subprocess.Popen("grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}'", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = process.communicate()
         if stderr:
-            rospy.err(stderr)
+            rospy.logerr(stderr)
         if stdout:
             setattr(health_msg, "cpu_usage_" + self.hostname, float(stdout))
         else:
@@ -164,7 +163,7 @@ class BoxStatus:
         process = subprocess.Popen("df -H --output=avail ${HOME} | awk 'NR==2 {print $1}'", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = process.communicate()
         if stderr:
-            rospy.err(stderr)
+            rospy.logerr(stderr)
         if stdout:
             setattr(health_msg, "avail_memory_" + self.hostname, stdout)
         else:
