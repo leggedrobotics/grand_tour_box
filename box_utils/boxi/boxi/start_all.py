12109c3d8782a39912f005aee94d613e61007807
Benjamin <benjamin.krummenacher@outlook.com>
2023-09-29 12:41:58 +0200

    update start all to allow to chose the calibration mode

--- box_utils/boxi/boxi/start_all.py
+++ box_utils/boxi/boxi/start_all.py
@@ -2,7 +2,9 @@ from boxi import BOX_ROOT_DIR, shell_run
 import socket
 
 def add_arguments(parser):
+    modes = ["no", "camera", "lidar", "imu"]
     parser.set_defaults(main=main)
+    parser.add_argument("-m", choices=modes, help="calibration mode of the box", default="no")
     return parser
 
 
@@ -11,12 +13,12 @@ def main(args):
     hostname = socket.gethostname()
 
     for host in hosts:
-        print("start ros on", host)
+        print("start ros in mode", args.m,"on", host)
 
         if host == hostname:
             cmd = f"tmuxp load $(rospack find box_launch)/tmux/box_" + host + ".yaml"
         else:
-            cmd = f"ssh -o ConnectTimeout=4 -t tmuxp load $(rospack find box_launch)/tmux/box_" + host + ".yaml"
+            cmd = f"ssh -o ConnectTimeout=4 -t tmuxp load $(rospack find box_launch)/tmux/box_" + host + "_calib_" + args.m + ".yaml"
         try:
             shell_run(cmd)
         except:
