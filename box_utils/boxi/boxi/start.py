479498c947375db4faa5eb5e285a30a03c885692
Benjamin <benjamin.krummenacher@outlook.com>
2023-10-06 14:38:34 +0200

    seperate start and end commands

--- box_utils/boxi/boxi/start.py
+++ box_utils/boxi/boxi/start.py
@@ -2,22 +2,15 @@ from boxi import BOX_ROOT_DIR, shell_run
 import argparse
 import os
 from pathlib import Path
+from time import sleep
 
 
 def add_arguments(parser):
     yamls = [s.stem for s in Path(BOX_ROOT_DIR).joinpath("box_bringup/box_launch/tmux").rglob("*.yaml")]
     parser.set_defaults(main=main)
     parser.add_argument("-c", choices=yamls, help="tmux configuration file name", default="box_opc")
-    parser.add_argument("--kill", action="store_true", help="Kill running tmux server")
     return parser
 
-
 def main(args):
-    if args.kill:
-        cmd = "tmux kill-server"
-        try:
-            shell_run(cmd)
-        except:
-            print("tmux wasn't running")
     cfg = os.path.join(BOX_ROOT_DIR, "box_bringup/box_launch/tmux", args.c + ".yaml")
-    shell_run(f"tmuxp load  {cfg}")
+    shell_run(f"tmuxp load  {cfg} -d")
--- box_utils/boxi/boxi/start_all.py
+++ box_utils/boxi/boxi/start_all.py
@@ -1,30 +1,31 @@
 from boxi import BOX_ROOT_DIR, shell_run
 import socket
 import logging as log
+from time import sleep
 
 def add_arguments(parser):
     modes = ["", "camera", "lidar", "imu"]
     parser.set_defaults(main=main)
     parser.add_argument("-m", choices=modes, help="calibration mode of the box", default="")
-    parser.add_argument("--sync_clocks", action="store_true", help="Sync clocks before starting ros")
     return parser
-
 
 def main(args):
     hosts = ["opc", "jetson", "nuc"]
     hostname = socket.gethostname()
 
-    if args.sync_clocks:
-        cmd = f'boxi initial_clock_sync'
-        shell_run(cmd)
+    mode = ""
+    if args.m == "no":
+        mode = ""
+    elif args.m:
+        mode = "_calib_" + args.m
 
     for host in hosts:
-        print("start ros in mode \"" + str(args.m) + "\" on", host)
+        print("Start ros in calibration mode \"" + str(args.m) + "\" on", host)
         if host == hostname:
-            cmd = f"tmuxp load $(rospack find box_launch)/tmux/box_" + host + "_calib_" + args.m + ".yaml -d"
+            cmd = f"boxi start -c box_" + host + mode
         else:
-            cmd = f"ssh -o ConnectTimeout=4 rsl@" + host + " -t /home/rsl/.local/bin/boxi start --kill -c box_" + host + "_calib_" + args.m
+            cmd = f"ssh -o ConnectTimeout=4 rsl@" + host + " -t /home/rsl/.local/bin/boxi start -c box_" + host + mode
         try:
             shell_run(cmd)
         except:
-            print("ros couldn't be started on", host)
+            print("ROS couldn't be started on", host)
