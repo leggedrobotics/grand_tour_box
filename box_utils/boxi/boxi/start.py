db2e3ff917ac7de69bc8220bd3381d4c9603cc82
kappi <kappi.patterson@gmail.com>
2024-03-04 17:43:11 +0100

    try connect to box wifi before starting

--- box_utils/boxi/boxi/start.py
+++ box_utils/boxi/boxi/start.py
@@ -1,6 +1,8 @@
 from boxi import BOX_ROOT_DIR, shell_run
 import argparse
+import configparser
 import os
+import subprocess
 from pathlib import Path
 from time import sleep
 
@@ -11,7 +13,25 @@ def add_arguments(parser):
     parser.add_argument("-c", choices=yamls, help="tmux configuration file name", default="box_opc")
     return parser
 
+# Use a .gitignored .ini file to store ssid and password. Example format:
+# [WiFi]
+# ssid=wifiNetworkName
+# password=wifiPassword
+def load_wifi_config(config_file='wifi_config.ini'):
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    return config['WiFi']['ssid'], config['Wifi']['password']
+
+def connect_to_wifi(ssid, password):
+    print(f"Attempting to connect to Grand Tour Box router.")
+    try:
+        subprocess.check_call(["nmcli", "dev", "wifi", "connect", ssid, "password", password])
+        print("Successfully connected")
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to connect. Error: {e}")
 
 def main(args):
+    ssid, password = load_wifi_config()
+    connect_to_wifi(ssid, password)
     cfg = os.path.join(BOX_ROOT_DIR, "box_launch/tmux", args.c + ".yaml")
     shell_run(f"tmuxp load  {cfg} -d")
