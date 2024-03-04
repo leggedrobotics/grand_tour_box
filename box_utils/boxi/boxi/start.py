33690aa5d2606bf70fd38bd28d3b7972b9ac43f5
Benjamin <benjamin.krummenacher@outlook.com>
2024-03-04 19:11:51 +0100

    error handling

--- box_utils/boxi/boxi/start.py
+++ box_utils/boxi/boxi/start.py
@@ -21,8 +21,8 @@ def add_arguments(parser):
 def load_wifi_config(config_file='wifi_config.ini'):
     config_file_path = os.path.expanduser('~/'+config_file)
     if not os.path.exists(config_file_path):
-        print("Place a wifi_config.ini file in your user home directory.")
-        sys.exit(1)
+        print("To autoconnect, place a wifi_config.ini file in your user home directory.")
+        return None, None
     config = configparser.ConfigParser()
     config.read(config_file_path)
     return config['WiFi']['ssid'], config['WiFi']['password']
@@ -33,10 +33,11 @@ def connect_to_wifi(ssid, password):
         subprocess.check_call(["nmcli", "dev", "wifi", "connect", ssid, "password", password])
         print("Successfully connected")
     except subprocess.CalledProcessError as e:
-        print(f"Failed to connect. Error: {e}")
+        print(f"Failed to connect via WiFi. Connect manually or with ethernet. Error: {e}")
 
 def main(args):
     ssid, password = load_wifi_config()
-    connect_to_wifi(ssid, password)
+    if ssid and password:
+        connect_to_wifi(ssid, password)
     cfg = os.path.join(BOX_ROOT_DIR, "box_launch/tmux", args.c + ".yaml")
     shell_run(f"tmuxp load  {cfg} -d")
