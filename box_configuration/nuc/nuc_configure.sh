4abe21e50ce545e4191e22c516db3f958f0ed035
Benjamin <benjamin.krummenacher@outlook.com>
2024-03-28 13:17:53 +0100

    udev rules for consistent camera assignment

--- box_configuration/nuc/nuc_configure.sh
+++ box_configuration/nuc/nuc_configure.sh
@@ -4,19 +4,19 @@
 echo '' >> ~/.bashrc
 echo 'source /opt/ros/noetic/setup.bash' >> ~/.bashrc
 echo 'source catkin_ws/devel/setup.bash' >> ~/.bashrc
-echo 'source catkin_ws/src/grand_tour_box/box_utils/box_setup/alias/alias.sh' >> ~/.bashrc
+echo 'source git/grand_tour_box/box_utils/box_setup/alias/alias.sh' >> ~/.bashrc
 
 # netplan
-cp /home/rsl/catkin_ws/src/grand_tour_box/box_configuration/nuc/01-network-manager-all.yaml /etc/netplan/01-network-manager-all.yaml
+cp /home/rsl/git/grand_tour_box/box_configuration/nuc/01-network-manager-all.yaml /etc/netplan/01-network-manager-all.yaml
 sudo netplan generate
 sudo netplan apply
 
 # autostart tmux & ptp
-cp /home/rsl/catkin_ws/src/grand_tour_box/box_configuration/nuc/autostart_tmux.service /etc/systemd/system/autostart_tmux.service
-cp /home/rsl/catkin_ws/src/grand_tour_box/box_configuration/nuc/phc2sys_NIC.service /lib/systemd/system/phc2sys_NIC.service
-cp /home/rsl/catkin_ws/src/grand_tour_box/box_configuration/nuc/phc2sys_system.service /lib/systemd/system/phc2sys_system.service
-cp /home/rsl/catkin_ws/src/grand_tour_box/box_configuration/nuc/ptp4l_enp45s0.service /lib/systemd/system/ptp4l_enp45s0.service
-cp /home/rsl/catkin_ws/src/grand_tour_box/box_configuration/nuc/ptp4l_enp46s0.service /lib/systemd/system/ptp4l_enp46s0.service
+cp /home/rsl/git/grand_tour_box/box_configuration/nuc/autostart_tmux.service /etc/systemd/system/autostart_tmux.service
+cp /home/rsl/git/grand_tour_box/box_configuration/nuc/phc2sys_NIC.service /lib/systemd/system/phc2sys_NIC.service
+cp /home/rsl/git/grand_tour_box/box_configuration/nuc/phc2sys_system.service /lib/systemd/system/phc2sys_system.service
+cp /home/rsl/git/grand_tour_box/box_configuration/nuc/ptp4l_enp45s0.service /lib/systemd/system/ptp4l_enp45s0.service
+cp /home/rsl/git/grand_tour_box/box_configuration/nuc/ptp4l_enp46s0.service /lib/systemd/system/ptp4l_enp46s0.service
 sudo systemctl daemon-reload
 sudo enable autostart_tmux
 sudo enable phc2sys_NIC
@@ -24,5 +24,6 @@ sudo enable phc2sys_system
 sudo enable ptp4l_enp45s0
 sudo enable ptp4l_enp46s0
 
-cp /home/rsl/catkin_ws/src/grand_tour_box/box_configuration/nuc/99-GMSL-kit.rules /etc/udev/rules.d/
+cp /home/rsl/git/grand_tour_box/box_configuration/nuc/99-GMSL-kit.rules /etc/udev/rules.d/
+cp /home/rsl/git/grand_tour_box/box_configuration/nuc/99-hdr-camera.rules /etc/udev/rules.d/
  udevadm control --reload && udevadm trigger
