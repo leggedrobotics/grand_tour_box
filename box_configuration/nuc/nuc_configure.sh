953419f29a3e46d5437f9980fd08904b8d12a8bd
Jonas Frey <jonfrey@ethz.ch>
2024-04-13 10:15:04 +0200

    ptp installation

--- box_configuration/nuc/nuc_configure.sh
+++ box_configuration/nuc/nuc_configure.sh
@@ -12,17 +12,17 @@ sudo netplan generate
 sudo netplan apply
 
 # autostart tmux & ptp
-cp /home/rsl/git/grand_tour_box/box_configuration/nuc/autostart_tmux.service /etc/systemd/system/autostart_tmux.service
-cp /home/rsl/git/grand_tour_box/box_configuration/nuc/phc2sys_NIC.service /lib/systemd/system/phc2sys_NIC.service
-cp /home/rsl/git/grand_tour_box/box_configuration/nuc/phc2sys_system.service /lib/systemd/system/phc2sys_system.service
-cp /home/rsl/git/grand_tour_box/box_configuration/nuc/ptp4l_enp45s0.service /lib/systemd/system/ptp4l_enp45s0.service
-cp /home/rsl/git/grand_tour_box/box_configuration/nuc/ptp4l_enp46s0.service /lib/systemd/system/ptp4l_enp46s0.service
+sudo cp /home/rsl/git/grand_tour_box/box_configuration/nuc/autostart_tmux.service /etc/systemd/system/autostart_tmux.service
+sudo cp /home/rsl/git/grand_tour_box/box_configuration/nuc/phc2sys_enp45s0.service /lib/systemd/system/phc2sys_enp45s0.service
+sudo cp /home/rsl/git/grand_tour_box/box_configuration/nuc/phc2sys_enp46s0.service /lib/systemd/system/phc2sys_enp46s0.service
+sudo cp /home/rsl/git/grand_tour_box/box_configuration/nuc/ptp4l_enp45s0.service /lib/systemd/system/ptp4l_enp45s0.service
+sudo cp /home/rsl/git/grand_tour_box/box_configuration/nuc/ptp4l_enp46s0.service /lib/systemd/system/ptp4l_enp46s0.service
 sudo systemctl daemon-reload
-sudo enable autostart_tmux
-sudo enable phc2sys_NIC
-sudo enable phc2sys_system
-sudo enable ptp4l_enp45s0
-sudo enable ptp4l_enp46s0
+sudo systemctl enable autostart_tmux
+sudo systemctl enable phc2sys_enp45s0
+sudo systemctl enable phc2sys_enp46s0
+sudo systemctl enable ptp4l_enp45s0
+sudo systemctl enable ptp4l_enp46s0
 
 cp /home/rsl/git/grand_tour_box/box_configuration/nuc/99-GMSL-kit.rules /etc/udev/rules.d/
 cp /home/rsl/git/grand_tour_box/box_configuration/nuc/99-hdr-camera.rules /etc/udev/rules.d/
