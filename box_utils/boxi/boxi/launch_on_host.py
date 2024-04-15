37578569caa73e54dd4d85cd3d31a0426722d8ce
Jonas Frey <jonfrey@ethz.ch>
2024-04-15 12:57:39 +0200

    created default mode recording

--- box_utils/boxi/boxi/launch_on_host.py
+++ box_utils/boxi/boxi/launch_on_host.py
@@ -8,7 +8,7 @@ from pathlib import Path
 def add_arguments(parser):
     yamls = [s.stem for s in Path(BOX_ROOT_DIR).joinpath("box_launch/tmux").rglob("*.yaml")]
     parser.set_defaults(main=main)
-    parser.add_argument("-c", choices=yamls, help="tmux configuration file name", default="box_opc")
+    parser.add_argument("-c", choices=yamls, help="tmux configuration file name", default="opc_recording")
     return parser
 
 
