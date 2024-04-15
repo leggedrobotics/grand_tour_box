3bd369878ada2b2624eab530a65729894a8e39b6
Jonas Frey <jonfrey@ethz.ch>
2024-04-15 18:31:54 +0200

    added catkin build for simple building

--- box_utils/boxi/boxi/catkin_build.py
+++ box_utils/boxi/boxi/catkin_build.py
@@ -5,10 +5,44 @@ import socket
 
 def add_arguments(parser):
     parser.set_defaults(main=main)
+    parser.add_argument("--jetson", action="store_true", help="Build on Jetson")
+    parser.add_argument("--opc", action="store_true", help="Build on OPC")
+    parser.add_argument("--nuc", action="store_true", help="Build on Nuc")
+    parser.add_argument("--pi", action="store_true", help="Build on Pi")
+    parser.add_argument("--all", action="store_true", help="Build All")
+    parser.add_argument("--clean", action="store_true", help="Clean before building")
     return parser
 
 
 def main(args):
-    host = socket.gethostname()
-    cmd = f"cd ~/catkin_ws; catkin build launch_{host}"
-    shell_run(cmd)
+    if args.all:
+        hosts = ["opc", "jetson", "nuc", "pi"]
+    else:
+        hosts = []
+        if args.jetson:
+            hosts.append("jetson")
+        if args.opc:
+            hosts.append("opc")
+        if args.nuc:
+            hosts.append("nuc")
+        if args.pi:
+            hosts.append("pi")
+        if len(hosts) == 0:
+            hosts.append(socket.gethostname())
+
+    hostname = socket.gethostname()
+    for host in hosts:
+        cmd = ""
+        if host == hostname:
+            if args.clean:
+                cmd += f"cd ~/catkin_ws; catkin clean --all -y; "
+            cmd += f"cd ~/catkin_ws; catkin build launch_{host}"
+        else:
+            cmd += f"ssh -o ConnectTimeout=4 rsl@{host} -t /home/rsl/.local/bin/boxi catkin_build --{host}"
+            if args.clean:
+                cmd =+ " --clean"
+
+        try:
+            shell_run(cmd)
+        except:
+            print(f"Failed to exectued command on {host}")
