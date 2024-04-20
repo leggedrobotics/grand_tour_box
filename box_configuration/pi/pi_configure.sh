d93dcf623b0dae21c47fc6606e34a5b2dd62aa26
Jonas Frey <jonasfrey96@gmail.com>
2024-04-20 11:38:21 +0200

    updated setup and fan control

--- box_configuration/pi/pi_configure.sh
+++ box_configuration/pi/pi_configure.sh
@@ -2,12 +2,31 @@ sudo apt-get -y install linuxptp
 
 sudo cp ~/git/grand_tour_box/box_configuration/pi/phc2sys_eth0.service /lib/systemd/system/
 sudo cp ~/git/grand_tour_box/box_configuration/pi/ptp4l_eth0.service /lib/systemd/system/
+
+
 sudo systemctl daemon-reload
 systemctl start phc2sys_eth0.service
 systemctl start ptp4l_eth0.service
+
+systemctl enable phc2sys_eth0.service
+systemctl enable ptp4l_eth0.service
+
 # systemctl status phc2sys_eth0.service
 # systemctl status ptp4l_eth0.service
 # systemctl restart phc2sys_eth0.service; systemctl restart ptp4l_eth0.service
 
 # Disable time sync to internet
 sudo timedatectl set-ntp no
+
+
+# Install pigpio
+wget https://github.com/joan2937/pigpio/archive/master.zip
+unzip master.zip
+cd pigpio-master
+make
+sudo make install
+
+# These will be added by default when installing pigpio
+sudo systemctl enable pigpiod
+sudo systemctl start pigpiod
+
