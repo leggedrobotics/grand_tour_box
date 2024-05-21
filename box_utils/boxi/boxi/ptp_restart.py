7b35a5d1ac25948a7065ce5e0b566320c4a27417
Jonas Frey <jonfrey@ethz.ch>
2024-05-21 11:06:13 +0200

    removed dual master option

--- box_utils/boxi/boxi/ptp_restart.py
+++ box_utils/boxi/boxi/ptp_restart.py
@@ -9,7 +9,6 @@ def add_arguments(parser):
     parser.add_argument("--nuc", action="store_true", help="Build on Nuc")
     parser.add_argument("--pi", action="store_true", help="Build on Pi")
     parser.add_argument("--all", action="store_true", help="Build All")
-    parser.add_argument("--grandmaster", choices=["jetson", "cpt7"], default="cpt7", help="Clean before building")
     return parser
 
 
@@ -32,55 +31,21 @@ def main(args):
     for host in  hosts:    
         if host == LOCAL_HOSTNAME:
             cmds = []
-            if host == "jetson":
-                if args.grandmaster == "jetson":
-                    print("Using OPC time as reference for Jetson")
-                    shell_run("remote_time=$(ssh rsl@opc date --iso=ns); echo rsl | sudo -S date -s $remote_time") 
-                    shell_run("echo rsl | sudo -S /usr/sbin/hwclock --systohc")
-                    cmds.append("/usr/bin/systemctl disable ptp4l_mgbe0.service")
-                    cmds.append("/usr/bin/systemctl disable phc2sys_mgbe0.service")  
-                    
-                    cmds.append("/usr/bin/systemctl enable ptp4l_mgbe0_grandmaster.service")
-                    cmds.append("/usr/bin/systemctl start ptp4l_mgbe0_grandmaster.service")
-                    cmds.append("/usr/bin/systemctl restart ptp4l_mgbe0_grandmaster.service")
-                    cmds.append("/usr/bin/systemctl enable phc2sys_mgbe0_grandmaster.service")
-                    cmds.append("/usr/bin/systemctl start phc2sys_mgbe0_grandmaster.service")
-                    cmds.append("/usr/bin/systemctl restart phc2sys_mgbe0_grandmaster.service")
-                    
-                    
-                elif args.grandmaster == "cpt7":
-                    cmds.append("/usr/bin/systemctl stop ptp4l_mgbe0_grandmaster.service")
-                    cmds.append("/usr/bin/systemctl disable ptp4l_mgbe0_grandmaster.service")
-                    cmds.append("/usr/bin/systemctl stop phc2sys_mgbe0_grandmaster.service")
-                    cmds.append("/usr/bin/systemctl disable phc2sys_mgbe0_grandmaster.service")
-                    
-                    cmds.append("/usr/bin/systemctl stop ptp4l_mgbe0.service")
-                    cmds.append("/usr/bin/systemctl stop phc2sys_mgbe0.service")
-                    
-                    cmds.append("/usr/bin/systemctl enable phc2sys_mgbe0.service")
-                    cmds.append("/usr/bin/systemctl enable ptp4l_mgbe0.service; sleep 1")
-                    
-                    cmds.append("/usr/bin/systemctl start phc2sys_mgbe0.service")          
-                    cmds.append("/usr/bin/systemctl start ptp4l_mgbe0.service")
-                    
-                    cmds.append("/usr/bin/systemctl restart phc2sys_mgbe0.service")          
-                    cmds.append("/usr/bin/systemctl restart ptp4l_mgbe0.service")
-                    
-
-                else:
-                    raise ValueError("Please provide a valid grandmaster host --grandmaster=[jetson, cpt7]")
+            if host == "jetson":                
+                cmds.append("/usr/bin/systemctl restart ptp4l_mgbe0.service; sleep 1")
+                cmds.append("/usr/bin/systemctl restart phc2sys_mgbe0.service")          
                 
-                cmds.append("/usr/bin/systemctl restart ptp4l_mgbe1.service")
+                cmds.append("/usr/bin/systemctl restart ptp4l_mgbe1.service; sleep 1")
                 cmds.append("/usr/bin/systemctl restart phc2sys_mgbe1.service")
 
             if host == "nuc":
-                cmds.append("/usr/bin/systemctl restart ptp4l_enp45s0.service")
-                cmds.append("/usr/bin/systemctl restart ptp4l_enp46s0.service")
+                cmds.append("/usr/bin/systemctl restart ptp4l_enp45s0.service; sleep 1")
                 cmds.append("/usr/bin/systemctl restart phc2sys_enp45s0.service")
-                cmds.append("/usr/bin/systemctl restart phc2sys_enp46s0.service")
 
+                cmds.append("/usr/bin/systemctl restart ptp4l_enp46s0.service; sleep 1")
+                cmds.append("/usr/bin/systemctl restart phc2sys_enp46s0.service")
             if host == "pi":
-                cmds.append("/usr/bin/systemctl restart ptp4l_eth0.service")
+                cmds.append("/usr/bin/systemctl restart ptp4l_eth0.service; sleep 1")
                 cmds.append("/usr/bin/systemctl restart phc2sys_eth0.service")
             
 
@@ -90,8 +55,8 @@ def main(args):
                 shell_run(cmd)
              
             if host == "jetson":
-                print ("Waiting for jetson to restart - 30 seconds")
-                time.sleep(30)
+                print ("Waiting for jetson to restart - 10 seconds")
+                time.sleep(10)
                 
         else:
             cmd = f"ssh -o ConnectTimeout=4 -t rsl@{host} /home/rsl/.local/bin/boxi ptp_restart --{host} --grandmaster={args.grandmaster} "
