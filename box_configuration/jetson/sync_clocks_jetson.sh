f7aa8cbca58a9b2b173ac504a3eaa1efa2674ec3
kappi <kpatterson@ethz.ch>
2024-06-26 22:03:39 +0200

    Adding sleeps and reverting password skip

--- box_configuration/jetson/sync_clocks_jetson.sh
+++ box_configuration/jetson/sync_clocks_jetson.sh
@@ -4,11 +4,15 @@ echo "Before:"
 date
 echo "Restarting ptp4l_mgbe0, ptp4l_mgbe1, phc2sys_mgbe0, phc2sys_mgbe1"
 sudo systemctl restart ptp4l_mgbe0
+sleep 0.5
 sudo systemctl restart ptp4l_mgbe1
+sleep 10
 sudo systemctl restart phc2sys_mgbe0
+sleep 0.5
 sudo systemctl restart phc2sys_mgbe1
 sleep 2
 echo "Restarting chrony.service"
 sudo systemctl restart chrony.service
+sleep 5
 echo "After:"
 date
