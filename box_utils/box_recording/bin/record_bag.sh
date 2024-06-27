776dfaca97e73e24e8cc1f1c94cc0dab8c0b2e3a
Jonas Frey <jonfrey@ethz.ch>
2024-06-27 18:49:03 +0200

    added compression back

--- box_utils/box_recording/bin/record_bag.sh
+++ box_utils/box_recording/bin/record_bag.sh
@@ -3,5 +3,4 @@
 path=$1
 topics="${@:2}"
 bagfile="${path}".bag
-#  --lz4
-rosbag record --split --duration=5m -O "${bagfile}" $topics
+rosbag record --split --lz4 --duration=5m -O "${bagfile}" $topics
