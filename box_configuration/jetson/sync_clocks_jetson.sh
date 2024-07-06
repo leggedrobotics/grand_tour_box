8e87825865949eaec7f02d9fc77abb2612f359cf
kappi <kpatterson@ethz.ch>
2024-07-06 09:57:59 +0200

    updated chrony cfg

--- box_configuration/jetson/sync_clocks_jetson.sh
+++ box_configuration/jetson/sync_clocks_jetson.sh
@@ -2,7 +2,7 @@
 
 echo "Before:"
 date
-echo "Restarting ptp4l_mgbe0, ptp4l_mgbe1, phc2sys_mgbe0, phc2sys_mgbe1"
+echo "Restarting ptp4l_mgbe0, ptp4l_mgbe1, phc2sys_mgbe0, phc2sys_mgbe1, box_chrony"
 sudo systemctl restart ptp4l_mgbe0
 sleep 0.5
 sudo systemctl restart ptp4l_mgbe1
@@ -12,7 +12,7 @@ sleep 0.5
 sudo systemctl restart phc2sys_mgbe1
 sleep 2
 echo "Restarting chrony.service"
-sudo systemctl restart chrony.service
+sudo systemctl restart box_chrony
 sleep 5
 echo "After:"
 date
--- box_configuration/opc/config.sh
+++
@@ -1,2 +0,0 @@
-cd ~/catkin_ws/src/grand_tour_box/box_drivers
-rosdep install -i --as-root pip:false --reinstall --from-paths multimaster_fkie
