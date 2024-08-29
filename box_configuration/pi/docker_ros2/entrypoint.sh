a838397cdeac99fc68ac9232b48b22607f02e687
Jonas Frey <jonfrey@ethz.ch>
2024-08-29 14:54:31 +0200

    updated docker files

--- box_configuration/pi/docker_ros2/entrypoint.sh
+++ box_configuration/pi/docker_ros2/entrypoint.sh
@@ -2,19 +2,13 @@
 
 figlet Welcome Docker ROS2
 
-
 export HOME=/home/$HOST_USERNAME
 export USER=$HOST_USERNAME
 cd $HOME
 
 # Enable sudo access without password
-echo "root ALL=(ALL) NOPASSWD:ALL" >> sudo /etc/sudoers
+echo "root ALL=(ALL) NOPASSWD:ALL" >> sudo v
 echo "$HOST_USERNAME ALL=(ALL) NOPASSWD:ALL" >> sudo /etc/sudoers
-
-export HOSTNAME
-export DISPLAY=:0.0
-
-export PS1="\e[0;34m(docker ROS2) \e[0;32m\u@\h:\e[0;34m\W\$ \e[0m"
 
 # Proceed as host user with superuser permissions
 if [ $# -gt 0 ]; then
