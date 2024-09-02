7017188c9c638519a8c21c504a27e95758fe5eb8
Jonas Frey <jonfrey@ethz.ch>
2024-09-02 12:59:05 +0200

    make build file more generic

--- box_utils/box_auto/docker/build.sh
+++ box_utils/box_auto/docker/build.sh
@@ -1,3 +1,3 @@
 # Adapt the ssh-key and the paths
 export DOCKER_BUILDKIT=1
-docker build --no-cache --progress=plain --ssh default=/home/jonfrey/.ssh/id_rsa_2 -t leggedrobotics/box -f /home/jonfrey/git/grand_tour_box/box_utils/box_auto/docker/box.Dockerfile "/home/jonfrey/git/grand_tour_box/box_utils/box_auto/docker"
+docker build --progress=plain --ssh default=$HOME/.ssh/id_rsa_2 -t leggedrobotics/box -f $HOME/git/grand_tour_box/box_utils/box_auto/docker/box.Dockerfile "$HOME/git/grand_tour_box/box_utils/box_auto/docker"
