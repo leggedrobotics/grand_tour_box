41a513b93c4991709a8e80fc605d27ace12f645b
Jonas Frey <jonfrey@ethz.ch>
2024-09-14 19:09:18 +0200

    fix hesai and health status

--- box_configuration/opc/alias.sh
+++ box_configuration/opc/alias.sh
@@ -4,6 +4,7 @@ alias tk='tmux kill-server'
 alias tk_dev='tmux kill-session -a'
 alias box='terminator -l box && exit'
 alias robot='terminator -l box_robot_new && exit'
+alias auto='terminator -l box_auto && exit'
 alias reset_chrony='sudo systemctl restart chrony.service'
 
 alias l-opc='tmuxp load $(rospack find box_launch)/../tmux/opc_recording.yaml'
