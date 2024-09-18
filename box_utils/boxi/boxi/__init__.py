4a522b2942a23dbab0cbf0458206b4532ead66f4
Frank Fu <fulkast@gmail.com>
2024-09-18 16:04:01 +0100

    Move color logger to boxi

--- box_utils/boxi/boxi/__init__.py
+++ box_utils/boxi/boxi/__init__.py
@@ -1,6 +1,9 @@
+import logging
 import os
-from pathlib import Path
 import socket
+from pathlib import Path
+
+import colorlog
 
 
 class bcolors:
@@ -24,3 +27,35 @@ from .os_utils import shell_run
 
 LOCAL_HOSTNAME = socket.gethostname()
 USERNAME = "rsl"
+
+
+class ColorLogger:
+    GREEN_CHECK = "\u2714"
+    YELLOW_WARNING = "\u26A0"
+    RED_CROSS = "\u274C"
+
+    @classmethod
+    def get_logger(cls):
+        # Define the log format with colors and emojis
+        log_format = (
+            '%(log_color)s%(levelname)s: %(message)s'
+        )
+
+        # Create a color mapping
+        colors = {
+            'DEBUG': 'cyan',
+            'INFO': 'green',
+            'WARNING': 'yellow',
+            'ERROR': 'red',
+            'CRITICAL': 'red,bg_white'
+        }
+
+        # Create a handler that writes log messages to the console
+        handler = colorlog.StreamHandler()
+        handler.setFormatter(colorlog.ColoredFormatter(log_format, log_colors=colors))
+
+        # Get the root logger and set its handler and log level
+        logger = colorlog.getLogger()
+        logger.addHandler(handler)
+        logger.setLevel(logging.DEBUG)
+        return logger
