0036cd13172c4929a9d963762a6d79b4e4f6f0fe
Johann Schwabe <johann@schwabe.dev>
2024-09-30 17:36:42 +0200

    fixed klein command

--- box_utils/box_auto/scripts/merge_mission.py
+++ box_utils/box_auto/scripts/merge_mission.py
@@ -57,7 +57,7 @@ def merge_bags_single(input_bag, output_bag, topics="*", verbose=False):
         )
 
     print(f"Uploading with klein {output_bag} to {os.environ['MISSION_UUID']}")
-    os.system(f"klein mission upload --mission {os.environ['MISSION_UUID']} --file {output_bag}")
+    os.system(f"klein mission upload --mission {os.environ['MISSION_UUID']} --path {output_bag}")
     os.system(f"cp {output_bag} /out")
     return total_included_count, total_skipped_count
 
