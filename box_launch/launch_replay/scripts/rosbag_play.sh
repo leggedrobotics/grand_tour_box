c9b18f5faa03ac79eefb35d200e9a24bcf19d019
Jonas Frey <jonfrey@ethz.ch>
2024-10-03 09:35:56 +0200

    added replay package and zsh support

---
+++ box_launch/launch_replay/scripts/rosbag_play.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+folder="./"
+
+# Function to display usage
+usage() {
+    echo "Usage: $0 [--folder <path>]"
+    echo "  --folder: Specify the folder path to search for bag files (default: current folder )"
+    exit 1
+}
+
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --folder)
+            folder="$2"
+            shift 2
+            ;;
+        -h|--help)
+            usage
+            ;;
+        *)
+            echo "Unknown option: $1"
+            usage
+            ;;
+    esac
+done
+
+# Array of keys to filter
+keys=(
+    # "_lpc_"
+    # "_npc_"
+    # "_nuc_hesai_post_processed.bag"
+    "_jetson_ap20_synced.bag"
+    "_cpt7_raw_imu.bag"
+    "_cpt7_gps_optimized_trajectory.bag"
+    "_tf_static.bag"
+    "_nuc_utils.bag"
+    "_nuc_tf.bag"
+    "_nuc_livox.bag"
+    "_nuc_hesai.bag"
+    # "_nuc_cpt7.bag"
+    # "_nuc_alphasense.bag"
+    # "_jetson_utils.bag"
+    # "_jetson_stim.bag"
+    # "_jetson_ap20_aux.bag"
+    # "_jetson_adis.bag"
+    # "_jetson_zed2i_tf.bag"
+    # "_jetson_zed2i_proprioceptive.bag"
+    # "_jetson_zed2i_images.bag"
+    # "_jetson_zed2i_depth.bag"
+    # "_jetson_hdr_right_raw.bag"
+    # "_jetson_hdr_left_raw.bag"
+    # "_jetson_hdr_front_raw.bag"
+)
+
+# Find all .bag files and filter them
+filtered_files=()
+while IFS= read -r file; do
+    for key in "${keys[@]}"; do
+        if [[ "$file" == *"$key"* ]]; then
+            filtered_files+=("$file")
+            break
+        fi
+    done
+done < <(find "$folder" -type f -name "*.bag")
+
+# If no files found, exit
+if [ ${#filtered_files[@]} -eq 0 ]; then
+    echo "No matching bag files found in $folder"
+    exit 1
+fi
+
+# Set ROS parameter for simulation time
+rosparam set use_sim_time 1
+
+# Play the filtered bag files
+rosbag play --clock "${filtered_files[@]}"
--- box_utils/box_kleinkram/upload_mission.py
+++ box_utils/box_kleinkram/upload_mission.py
@@ -4,7 +4,41 @@ MISSION_DATA = os.environ.get("MISSION_DATA", "/mission_data")
 
 folder = Path(MISSION_DATA)
 files_to_upload = [str(s) for s in folder.rglob("*.bag")]
+keys = [
+    "_lpc_",
+    "_npc_",
+    "_nuc_hesai_post_processed.bag",
+    "_jetson_ap20_synced.bag",
+    "_cpt7_raw_imu.bag",
+    "_cpt7_gps_optimized_trajectory.bag",
+    "_tf_static.bag",
+    "_nuc_utils.bag",
+    "_nuc_tf.bag",
+    "_nuc_livox.bag",
+    "_nuc_hesai.bag",
+    "_nuc_cpt7.bag",
+    "_nuc_alphasense.bag",
+    "_jetson_utils.bag",
+    "_jetson_stim.bag",
+    "_jetson_ap20_aux.bag",
+    "_jetson_adis.bag",
+    "_jetson_zed2i_tf.bag",
+    "_jetson_zed2i_proprioceptive.bag",
+    "_jetson_zed2i_images.bag",
+    "_jetson_zed2i_depth.bag",
+    "_jetson_hdr_right_raw.bag",
+    "_jetson_hdr_left_raw.bag",
+    "_jetson_hdr_front_raw.bag",
+]
+upload = []
+for f in files_to_upload:
+    for k in keys:
+        if f.find(k) != -1:
+            upload.append(f)
+
+upload.sort()
+
 mission_name = folder.name
-project = "GrandTourShakeout"
-files_to_upload = ",".join(files_to_upload)
-os.system(f"klein upload --path {files_to_upload} --project {project} --mission {mission_name}")
+project = "GrandTourDev"
+files_to_upload = " --path " + " --path ".join(files_to_upload)
+os.system(f"klein upload {files_to_upload} --project {project} --mission {mission_name}")
