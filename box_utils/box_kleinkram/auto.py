8dbe5d4b99133e740325433ca84f8c325b0b5f8f
Jonas Frey <jonfrey@ethz.ch>
2024-10-25 19:13:01 +0200

    both tested

--- box_utils/box_kleinkram/auto.py
+++ box_utils/box_kleinkram/auto.py
@@ -1,77 +1,80 @@
-from boxi import shell_run, LOCAL_HOSTNAME
+import argparse
 import os
 from pathlib import Path
 import glob
+from boxi import shell_run, LOCAL_HOSTNAME
 
-MISSION_DATA = os.environ.get("MISSION_DATA", "/mission_data")
 
-cmds = []
-if LOCAL_HOSTNAME == "jetson":
-    cmds.append(
-        f"export MISSION_DATA={MISSION_DATA}; python3 /home/rsl/catkin_ws/src/grand_tour_box/box_utils/box_auto/scripts/mcap_to_rosbag.py"
-    )
-    zed_svo = sorted(glob.glob(os.path.join(MISSION_DATA, "*_jetson_zed2i.svo2")))[0]
-    cmds.append(
-        f"export MISSION_DATA={MISSION_DATA}; source /home/rsl/catkin_ws/devel/setup.bash; roslaunch zed_wrapper zed2i_replay.launch svo_file:={zed_svo}"
-    )
-    cmds.append(
-        f"export MISSION_DATA={MISSION_DATA}; python3 /home/rsl/catkin_ws/src/grand_tour_box/box_utils/box_auto/scripts/merge_mission.py"
-    )
-    keys = [
-        "_jetson_utils.bag",
-        "_jetson_stim.bag",
-        "_jetson_ap20_aux.bag",
-        "_jetson_adis.bag",
-        "_jetson_zed2i_tf.bag",
-        "_jetson_zed2i_proprioceptive.bag",
-        "_jetson_zed2i_images.bag",
-        "_jetson_zed2i_depth.bag",
-        "_jetson_hdr_right_raw.bag",
-        "_jetson_hdr_left_raw.bag",
-        "_jetson_hdr_front_raw.bag",
-    ]
+def process_mission_data(mission_name):
+    MISSION_DATA = f"/data/{mission_name}"
+    cmds = []
 
-elif LOCAL_HOSTNAME == "nuc":
-    cmds.append(
-        f"export MISSION_DATA={MISSION_DATA}; python3 /home/rsl/catkin_ws/src/grand_tour_box/box_utils/box_auto/scripts/merge_mission.py"
-    )
-    cmds.append(
-        f"export MISSION_DATA={MISSION_DATA}; python3 /home/rsl/catkin_ws/src/grand_tour_box/box_utils/box_auto/scripts/create_tf_static.py"
-    )
-    keys = [
-        "_tf_static.bag",
-        "_nuc_hesai_post_processed.bag",
-        "_nuc_utils.bag",
-        "_nuc_tf.bag",
-        "_nuc_livox.bag",
-        "_nuc_hesai.bag",
-        "_nuc_cpt7.bag",
-        "_nuc_alphasense.bag",
-    ]
+    if LOCAL_HOSTNAME == "jetson":
+        cmds.append(
+            f"export MISSION_DATA={MISSION_DATA}; python3 /home/rsl/catkin_ws/src/grand_tour_box/box_utils/box_auto/scripts/mcap_to_rosbag.py"
+        )
+        zed_svo = sorted(glob.glob(os.path.join(MISSION_DATA, "*_jetson_zed2i.svo2")))[0]
+        cmds.append(
+            f"export MISSION_DATA={MISSION_DATA}; source /home/rsl/catkin_ws/devel/setup.bash; roslaunch zed_wrapper zed2i_replay.launch svo_file:={zed_svo}"
+        )
+        cmds.append(
+            f"export MISSION_DATA={MISSION_DATA}; python3 /home/rsl/catkin_ws/src/grand_tour_box/box_utils/box_auto/scripts/merge_mission.py"
+        )
+        keys = [
+            "_jetson_utils.bag",
+            "_jetson_stim.bag",
+            "_jetson_ap20_aux.bag",
+            "_jetson_adis.bag",
+            "_jetson_zed2i_tf.bag",
+            "_jetson_zed2i_proprioceptive.bag",
+            "_jetson_zed2i_images.bag",
+            "_jetson_zed2i_depth.bag",
+            "_jetson_hdr_right_raw.bag",
+            "_jetson_hdr_left_raw.bag",
+            "_jetson_hdr_front_raw.bag",
+        ]
 
-for cmd in cmds:
-    shell_run(cmd)
+    elif LOCAL_HOSTNAME == "nuc":
+        cmds.append(
+            f"export MISSION_DATA={MISSION_DATA}; python3 /home/rsl/catkin_ws/src/grand_tour_box/box_utils/box_auto/scripts/merge_mission.py"
+        )
+        cmds.append(
+            f"export MISSION_DATA={MISSION_DATA}; python3 /home/rsl/catkin_ws/src/grand_tour_box/box_utils/box_auto/scripts/create_tf_static.py"
+        )
+        keys = [
+            "_tf_static.bag",
+            "_nuc_hesai_post_processed.bag",
+            "_nuc_utils.bag",
+            "_nuc_tf.bag",
+            "_nuc_livox.bag",
+            "_nuc_hesai.bag",
+            "_nuc_cpt7.bag",
+            "_nuc_alphasense.bag",
+        ]
 
-folder = Path(MISSION_DATA)
-files_to_upload = [str(s) for s in folder.rglob("*.bag")]
+    for cmd in cmds:
+        shell_run(cmd)
 
-# Bags that need to be added manually:
-# keys = [
-#     "_lpc_",
-#     "_npc_",
-#     "_cpt7_raw_imu.bag",
-#     "_cpt7_gps_optimized_trajectory.bag",
-# ]
+    folder = Path(MISSION_DATA)
+    files_to_upload = [str(s) for s in folder.rglob("*.bag")]
 
-upload = []
-for f in files_to_upload:
-    for k in keys:
-        if f.find(k) != -1:
-            upload.append(f)
+    upload = []
+    for f in files_to_upload:
+        for k in keys:
+            if k in f:
+                upload.append(f)
 
-upload.sort()
+    upload.sort()
 
-mission_name = folder.name
-project = "GrandTourDev"
-files_to_upload = " --path " + " --path ".join(files_to_upload)
-print(f"klein upload {files_to_upload} --project {project} --mission {mission_name}")
+    project = "GrandTourDev"
+    files_to_upload_str = " --path " + " --path ".join(upload)
+    print(f"klein upload {files_to_upload_str} --project {project} --mission {mission_name}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process and upload mission data.")
+    parser.add_argument("mission_names", nargs="+", help="One or more mission names to process.")
+    args = parser.parse_args()
+
+    for mission_name in args.mission_names:
+        process_mission_data(mission_name)
--- box_utils/boxi/boxi/get_data.py
+++ box_utils/boxi/boxi/get_data.py
@@ -9,7 +9,9 @@ def add_arguments(parser):
     parser.add_argument("--npc", action="store_true", help="Get data from Nuc")
     parser.add_argument("--check", action="store_true", help="Get data from Nuc")
     parser.add_argument("--post-only", action="store_true", help="Get data from Nuc")
-    parser.add_argument("--directory", type=str, required=True, help="Specific directory inside /data to sync")
+    parser.add_argument(
+        "--directory", type=str, nargs="+", required=True, help="One or more specific directories inside /data to sync"
+    )
     return parser
 
 
@@ -32,42 +34,43 @@ def main(args):
         hosts.append("npc")
         users.append("rsl")
 
-    if args.directory:
-        if args.post_only:
-            keys = [
-                "_jetson_utils.bag",
-                "_jetson_stim.bag",
-                "_jetson_ap20_aux.bag",
-                "_jetson_adis.bag",
-                "_jetson_zed2i_tf.bag",
-                "_jetson_zed2i_proprioceptive.bag",
-                "_jetson_zed2i_images.bag",
-                "_jetson_zed2i_depth.bag",
-                "_jetson_hdr_right.bag",
-                "_jetson_hdr_front.bag",
-                "_jetson_hdr_left.bag",
-                "_tf_static.bag",
-                "_nuc_hesai_post_processed.bag",
-                "_nuc_utils.bag",
-                "_nuc_tf.bag",
-                "_nuc_livox.bag",
-                "_nuc_hesai.bag",
-                "_nuc_cpt7.bag",
-                "_nuc_alphasense.bag",
-            ]
-            include_patterns = " ".join([f"--include='*{key}'" for key in keys])
-            rsync_part2 = f":/data/{args.directory}* . --include=*/ {include_patterns} --exclude='*' "
-        else:
-            rsync_part2 = f":/data/{args.directory} . {rsync_exclusions}"
+    for directory in args.directory:
+        if directory:
+            if args.post_only:
+                keys = [
+                    "_jetson_utils.bag",
+                    "_jetson_stim.bag",
+                    "_jetson_ap20_aux.bag",
+                    "_jetson_adis.bag",
+                    "_jetson_zed2i_tf.bag",
+                    "_jetson_zed2i_proprioceptive.bag",
+                    "_jetson_zed2i_images.bag",
+                    "_jetson_zed2i_depth.bag",
+                    "_jetson_hdr_right.bag",
+                    "_jetson_hdr_front.bag",
+                    "_jetson_hdr_left.bag",
+                    "_tf_static.bag",
+                    "_nuc_hesai_post_processed.bag",
+                    "_nuc_utils.bag",
+                    "_nuc_tf.bag",
+                    "_nuc_livox.bag",
+                    "_nuc_hesai.bag",
+                    "_nuc_cpt7.bag",
+                    "_nuc_alphasense.bag",
+                ]
+                include_patterns = " ".join([f"--include='*{key}'" for key in keys])
+                rsync_part2 = f":/data/{directory}* . --include=*/ {include_patterns} --exclude='*' "
+            else:
+                rsync_part2 = f":/data/{directory} . {rsync_exclusions}"
 
-    if len(hosts) == 0:
-        print("No host specified. Specify host with --hostname")
-    for host, user in zip(hosts, users):
-        if args.check:
-            rsync_part1 = "rsync -r --progress -Pv --size-only -n " + user + "@"
-        else:
-            rsync_part1 = "rsync -r --progress -Pv --size-only " + user + "@"
+            if len(hosts) == 0:
+                print("No host specified. Specify host with --hostname")
+            for host, user in zip(hosts, users):
+                if args.check:
+                    rsync_part1 = "rsync -r --progress -Pv --size-only -n " + user + "@"
+                else:
+                    rsync_part1 = "rsync -r --progress -Pv --size-only " + user + "@"
 
-        cmd = f"{rsync_part1}{host}{rsync_part2}"
-        print(cmd)
-        shell_run(cmd)
+                cmd = f"{rsync_part1}{host}{rsync_part2}"
+                print(cmd)
+                shell_run(cmd)
