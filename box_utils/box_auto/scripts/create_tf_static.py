e8e1e2e1393182dea5ccfd848fcd5c0276695bb9
JonasFrey96 <jonfrey@ethz.ch>
2024-10-31 16:56:43 +0100

    fixed another download bug

--- box_utils/box_auto/scripts/create_tf_static.py
+++ box_utils/box_auto/scripts/create_tf_static.py
@@ -71,7 +71,7 @@ def process_bags(reference_bag_path, tf_static_bag_path, output_bag_path, start_
 def main():
     if os.environ.get("KLEINKRAM_ACTIVE", False) == "ACTIVE":
         uuid = os.environ["MISSION_UUID"]
-        os.system(f"klein mission download --mission-uuid {uuid} --local-path /mission_data --pattern *_nuc_livox")
+        os.system(f"klein mission download --mission-uuid {uuid} --local-path /mission_data --pattern *_nuc_livox.bag")
 
     reference_bag_path, suc = get_bag(MISSION_DATA, "*_nuc_livox.bag")
     tf_static_bag_path = os.path.join(get_package_path("box_calibration"), "calibration/tf_static.bag")
