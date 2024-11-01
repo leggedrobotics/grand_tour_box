f0c52eacf9a8911cf636e5efdd71bb083407a95d
Jonas Frey <jonfrey@ethz.ch>
2024-11-01 13:46:22 +0100

    added

---
+++ box_utils/box_kleinkram/upload_calibration.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+import os
+
+MISSION_DATA = os.environ.get("MISSION_DATA", "/mission_data")
+
+folder = Path(MISSION_DATA)
+files_to_upload = [str(s) for s in folder.rglob("*.bag")]
+keys = [
+    ".bag",
+]
+upload = []
+for f in files_to_upload:
+    for k in keys:
+        if f.find(k) != -1:
+            upload.append(f)
+
+upload.sort()
+
+mission_name = folder.name
+project = "GrandTourCalibration"
+files_to_upload = " --path " + " --path ".join(files_to_upload)
+os.system(f"klein upload --create-mission {files_to_upload} --project {project} --mission {mission_name}")
