26840048ee4728334e51fde3b899653121190891
JonasFrey96 <jonfrey@ethz.ch>
2024-12-16 21:28:46 +0100

    fixed small bug

--- box_utils/box_auto/python/box_auto/scripts/general/merge_dlio_tf_static.py
+++ box_utils/box_auto/python/box_auto/scripts/general/merge_dlio_tf_static.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     dlio_bag = get_bag("*_dlio.bag")
     tf_static_bag = get_bag("*_tf_static_start_end.bag")
 
-    out_bag = dlio_bag.replace("dlio.bag", "_tf_static_dlio_tf.bag")
+    out_bag = dlio_bag.replace("_dlio.bag", "_tf_static_dlio_tf.bag")
 
     run_ros_command(
         f'python3 {BOX_AUTO_SCRIPTS_DIR}/general/merge_bags.py --input={dlio_bag},{tf_static_bag} --output={out_bag} --filter="/tf /tf_static"'
