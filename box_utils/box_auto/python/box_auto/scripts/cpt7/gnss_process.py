f1a7bc2d3621322932796234fa2d2aaad4853410
JonasFrey96 <jonfrey@ethz.ch>
2024-12-17 10:58:00 +0100

    updated box_auto for kk

--- box_utils/box_auto/python/box_auto/scripts/cpt7/gnss_process.py
+++ box_utils/box_auto/python/box_auto/scripts/cpt7/gnss_process.py
@@ -1,31 +1,40 @@
 import os
 from pathlib import Path
+from box_auto.utils import MISSION_DATA, get_file
 
-print("CLI REFERENCE: https://docs.novatel.com/Waypoint/Content/Appendix/WPGCMD.htm")
 
-for PROCMODE in ["tc", "lc", "ppp", "dgps"]:
+def main():
+    print("CLI REFERENCE: https://docs.novatel.com/Waypoint/Content/Appendix/WPGCMD.htm")
+    for PROCMODE in ["tc", "lc", "ppp", "dgps"]:
 
-    for EXP_PROFILE in ["GrandTour-LocalFrame-minimal", "GPX"]:
-        LOGFILE = "/media/jonfrey/Data/deployment_day_16/2024-11-25-14-57-08/NMZT24120004N_2024-11-25_14-03-31.LOG"
-        PROJ = str(Path(LOGFILE).parent / "ie" / PROCMODE / f"ie_{PROCMODE}.proj")
-        OUTPUT = str(Path(LOGFILE).parent / "ie" / f"output_{PROCMODE}_{EXP_PROFILE}.txt")
-        IE_API_KEY = os.environ["IE_API_KEY"]
-        Path(PROJ).parent.mkdir(exist_ok=True, parents=True)
+        for EXP_PROFILE in ["GrandTour-LocalFrame-minimal", "GPX"]:
+            LOGFILE, suc = get_file("*.LOG")
+            if not suc:
+                raise ValueError("Failed to find LOG file.")
 
-        cmd = "cd /home/jonfrey/Downloads/waypoint_ie_10_00_1206/bin; ./WPGCMDIMU "
+            PROJ = str(Path(MISSION_DATA) / "ie" / PROCMODE / f"ie_{PROCMODE}.proj")
+            OUTPUT = str(Path(MISSION_DATA) / "ie" / f"output_{PROCMODE}_{EXP_PROFILE}.txt")
+            IE_API_KEY = os.environ["IE_API_KEY"]
+            Path(PROJ).parent.mkdir(exist_ok=True, parents=True)
 
-        cmd += f'-remfile "{LOGFILE}" '
-        cmd += f"-procmode {PROCMODE} "
-        cmd += f"-proccfg {PROJ} "
-        cmd += '-procprofile "SPAN Pedestrian (CPT7-HG4930)" '
-        cmd += "-downloadbase 1 50 "
-        cmd += "-snserver ch-xpos.nrtk.eu "  # https://ch.nrtk.eu/sbc/ -> This needs to be inferred
-        cmd += "-procdatum WGS84 "
-        cmd += "-snusername RSL_02 "
-        cmd += f"-snpassword {IE_API_KEY} "
-        cmd += f"-expprofile1 {EXP_PROFILE} "
-        cmd += "-expsrc1 epochs "
-        cmd += f"-expfile1 {OUTPUT} "
+            cmd = "cd /home/jonfrey/Downloads/waypoint_ie_10_00_1206/bin; ./WPGCMDIMU "
 
-        print(cmd)
-        os.system(cmd)
+            cmd += f'-remfile "{LOGFILE}" '
+            cmd += f"-procmode {PROCMODE} "
+            cmd += f"-proccfg {PROJ} "
+            cmd += '-procprofile "SPAN Pedestrian (CPT7-HG4930)" '
+            cmd += "-downloadbase 1 50 "
+            cmd += "-snserver ch-xpos.nrtk.eu "  # https://ch.nrtk.eu/sbc/ -> This needs to be inferred
+            cmd += "-procdatum WGS84 "
+            cmd += "-snusername RSL_02 "
+            cmd += f"-snpassword {IE_API_KEY} "
+            cmd += f"-expprofile1 {EXP_PROFILE} "
+            cmd += "-expsrc1 epochs "
+            cmd += f"-expfile1 {OUTPUT} "
+
+            print(cmd)
+            os.system(cmd)
+
+
+if __name__ == "__main__":
+    main()
