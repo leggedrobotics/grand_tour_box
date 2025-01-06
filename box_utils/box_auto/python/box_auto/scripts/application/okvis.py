41d682d8f264f8fc7879f3ab127e8cd01fee7ebd
JonasFrey96 <jonfrey@ethz.ch>
2025-01-06 17:35:15 +0100

    added full documentation on how to run imu experiment

--- box_utils/box_auto/python/box_auto/scripts/application/okvis.py
+++ box_utils/box_auto/python/box_auto/scripts/application/okvis.py
@@ -4,6 +4,26 @@ from pathlib import Path
 import yaml
 import datetime
 
+# Assumes you have previously run the euroc_dataset_converter.py
+# After generating the dataset one has to still manually adjust the okvis config to match the desired profile
+# For running the IMU experiment we recommend copying the data for the different imus in subfolder
+# euroc_grand_tour
+#  |euroc_config.yaml
+#  |--alphasense_imu
+#      |--cpt7
+#         |--cam0
+#         |--cam1
+#         |--imu0
+#      |--stim
+#         |--cam0
+#         |--cam1
+#         |--imu0
+#      ....
+# For this create the subfolder e.g. "stim" and ensure to rename the correct imu in this case imu4
+# Refer for this to the euroc_config.yaml
+# Do not forget to manually modify the correct okvis_config_alphasense_2_bw_stim.yaml files
+
+
 if __name__ == "__main__":
     runs = {
         "alphasense_2_bw": {
@@ -41,6 +61,37 @@ if __name__ == "__main__":
         "zed_1_rgb": {
             "data_folder": "euroc_grand_tour/zed/",
             "okvis_config": "okvis_config_zed_1rgb.yaml",
+        },
+    }
+
+    runs_imu = {
+        "alphasense_2_bw_cpt7": {
+            "data_folder": "euroc_grand_tour/alphasense_imu/cpt7",
+            "okvis_config": "okvis_config_2_bw.yaml",
+        },
+        "alphasense_2_bw_alphasense": {
+            "data_folder": "euroc_grand_tour/alphasense_imu/alphasense",
+            "okvis_config": "okvis_config_2_bw.yaml",
+        },
+        "alphasense_2_bw_ap20": {
+            "data_folder": "euroc_grand_tour/alphasense_imu/ap20",
+            "okvis_config": "okvis_config_2_bw.yaml",
+        },
+        "alphasense_2_bw_zed": {
+            "data_folder": "euroc_grand_tour/alphasense_imu/zed",
+            "okvis_config": "okvis_config_2_bw.yaml",
+        },
+        "alphasense_2_bw_stim": {
+            "data_folder": "euroc_grand_tour/alphasense_imu/stim",
+            "okvis_config": "okvis_config_2_bw.yaml",
+        },
+        "alphasense_2_bw_adis": {
+            "data_folder": "euroc_grand_tour/alphasense_imu/adis",
+            "okvis_config": "okvis_config_2_bw.yaml",
+        },
+        "alphasense_2_bw_livox": {
+            "data_folder": "euroc_grand_tour/alphasense_imu/livox",
+            "okvis_config": "okvis_config_2_bw.yaml",
         },
     }
 
