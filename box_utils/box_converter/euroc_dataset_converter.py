65d7ef7c0666c1ad3592880f8c9d47709db5de5b
JonasFrey96 <jonfrey@ethz.ch>
2025-01-07 11:17:19 +0100

    single export for all cameras and imus

--- box_utils/box_converter/euroc_dataset_converter.py
+++ box_utils/box_converter/euroc_dataset_converter.py
@@ -13,7 +13,7 @@ import yaml
 bridge = CvBridge()
 
 
-def process_camera_data(topic, camera_info_topic, base_frame_id, bag_pattern, cam_folder, tf_lookup):
+def process_camera_data(topic, camera_info_topic, bag_pattern, cam_folder):
     data_folder = cam_folder / "data"
     data_folder.mkdir(exist_ok=True, parents=True)
     csv_path = str(cam_folder / "data.csv")
@@ -22,10 +22,6 @@ def process_camera_data(topic, camera_info_topic, base_frame_id, bag_pattern, ca
 
     with rosbag.Bag(bag_path, "r") as bag, open(csv_path, "w", newline="") as csvfile:
         for _topic, camera_info_msg, t in bag.read_messages(topics=[camera_info_topic]):
-            t_sensor_base, q_sensor_base = tf_lookup.lookupTransform(base_frame_id, camera_info_msg.header.frame_id, t)
-            T_sensor_base = np.eye(4)
-            T_sensor_base[:3, :3] = R.from_quat(q_sensor_base).as_matrix()
-            T_sensor_base[:3, 3] = np.array(t_sensor_base)
             break
 
         writer = csv.writer(csvfile)
@@ -41,7 +37,7 @@ def process_camera_data(topic, camera_info_topic, base_frame_id, bag_pattern, ca
 
             writer.writerow([timestamp, filename])
 
-    return T_sensor_base, camera_info_msg
+    return camera_info_msg
 
 
 def process_imu_data(topic, bag_pattern, imu_folder):
@@ -137,72 +133,82 @@ def process(rosbags, tag):
     # Configure input and output paths
     output_dir = Path(MISSION_DATA) / "euroc_grand_tour" / tag
     cams = [k for k in rosbags.keys() if "cam" in k]
+    imus = [k for k in rosbags.keys() if "imu" in k]
 
     # Integrate all the measurements
     tf_lookup = BagTfTransformer(get_bag(rosbags["tf_static"][1], rglob=False))
 
     # Process each bag
     infos = {}
-    imu_frame_id = "TBD"  # Will be automaticially infered - IMU is processed before camera
 
     for sensor, data in rosbags.items():
         folder = output_dir / sensor
 
         if "cam" in sensor:
             topic, camera_info_topic, bag_pattern = data
-            T_sensor_base, camera_info_msg = process_camera_data(
-                topic, camera_info_topic, imu_frame_id, bag_pattern, folder, tf_lookup
-            )
+            camera_info_msg = process_camera_data(topic, camera_info_topic, bag_pattern, folder)
             infos[sensor] = {
-                "T_sensor_base": T_sensor_base,
                 "camera_info_msg": camera_info_msg,
             }
         elif "imu" in sensor:
             topic, bag_pattern = data
             imu_msg = process_imu_data(topic, bag_pattern, folder)
-            imu_frame_id = imu_msg.header.frame_id
             infos[sensor] = {
                 "imu_msg": imu_msg,
             }
 
     print("Data conversion completed!")
 
-    # Create OKVIS config
-    template_file = Path(BOX_AUTO_DIR).parent / "box_converter" / "euroc" / f"okvis_{tag}.yaml"
-    updated_parameters = {
-        k: {
-            "T_SC": infos[k]["T_sensor_base"].flatten().tolist(),
-            "image_dimension": [infos[k]["camera_info_msg"].width, infos[k]["camera_info_msg"].height],
-            "distortion_coefficients": list(infos[k]["camera_info_msg"].D),
-            "focal_length": [infos[k]["camera_info_msg"].K[0], infos[k]["camera_info_msg"].K[4]],
-            "principal_point": [infos[k]["camera_info_msg"].K[2], infos[k]["camera_info_msg"].K[5]],
-            "distortion_type": infos[k]["camera_info_msg"].distortion_model.replace("radtan", "radialtangential"),
-        }
-        for k in cams
-    }
+    # Generate for each IMU an OKVIZ config
+    for imu in imus:
+        # Create OKVIS config
+        template_file = Path(BOX_AUTO_DIR).parent / "box_converter" / "euroc" / f"okvis_{tag}.yaml"
+        updated_parameters = []
 
-    # Load the template YAML
-    with open(template_file) as stream:
-        template_data = yaml.safe_load(stream)
+        for k in cams:
+            t_sensor_base, q_sensor_base = tf_lookup.lookupTransform(
+                infos[imu].header.frame_id, infos[k]["camera_info_msg"].header.frame_id
+            )
+            T_sensor_base = np.eye(4)
+            T_sensor_base[:3, :3] = R.from_quat(q_sensor_base).as_matrix()
+            T_sensor_base[:3, 3] = np.array(t_sensor_base)
 
-    # Update parameters
-    for i, cam_key in enumerate(cams):
-        for k in updated_parameters[cam_key].keys():
-            template_data["cameras"][i][k] = updated_parameters[cam_key][k]
+            updated_parameters[k] = {
+                "T_SC": T_sensor_base.flatten().tolist(),
+                "image_dimension": [infos[k]["camera_info_msg"].width, infos[k]["camera_info_msg"].height],
+                "distortion_coefficients": list(infos[k]["camera_info_msg"].D),
+                "focal_length": [infos[k]["camera_info_msg"].K[0], infos[k]["camera_info_msg"].K[4]],
+                "principal_point": [infos[k]["camera_info_msg"].K[2], infos[k]["camera_info_msg"].K[5]],
+                "distortion_type": infos[k]["camera_info_msg"].distortion_model.replace("radtan", "radialtangential"),
+            }
 
-    write_opencv_yaml(output_dir / "okvis_config.yaml", template_data)
-    print(f"Created yaml for OKVIS (imu not updated) {output_dir}/okvis_config.yaml")
+        # Load the template YAML
+        with open(template_file) as stream:
+            template_data = yaml.safe_load(stream)
 
-    euroc_cfg_path = str(output_dir / "euroc_config.yaml")
-    # Write the dictionary to the YAML file
-    with open(euroc_cfg_path, "w") as f:
-        yaml.dump(rosbags, f, default_flow_style=False)
+        # Update parameters
+        for i, cam_key in enumerate(cams):
+            for k in updated_parameters[cam_key].keys():
+                template_data["cameras"][i][k] = updated_parameters[cam_key][k]
+
+        write_opencv_yaml(output_dir / f"okvis_config_{imu}.yaml", template_data)
+        print(f"Created yaml for OKVIS (imu not updated) {output_dir}/okvis_config_{imu}.yaml")
+
+        euroc_cfg_path = str(output_dir / "euroc_config.yaml")
+        # Write the dictionary to the YAML file
+        with open(euroc_cfg_path, "w") as f:
+            yaml.dump(rosbags, f, default_flow_style=False)
 
 
 if __name__ == "__main__":
-    alphasense_cfg = {
+    all_cameras_cfg = {
         "imu0": ("/gt_box/cpt7/offline_from_novatel_logs/imu", "*_cpt7_raw_imu.bag"),
         "imu1": ("/gt_box/alphasense_driver_node/imu", "*_nuc_alphasense_calib.bag"),
+        "imu2": ("/gt_box/ap20/imu", "*_jetson_ap20_synced.bag"),
+        "imu3": ("/gt_box/zed2i/zed_node/imu_raw/data", "*_jetson_zed2i_prop.bag"),
+        "imu4": ("/gt_box/stim320/imu", "*_jetson_stim.bag"),
+        "imu5": ("/gt_box/adis16475_node/imu", "*_jetson_adis.bag"),
+        "imu6": ("/gt_box/livox/imu_si_compliant", "*_nuc_livox.bag"),
         "cam0": (
             "/gt_box/alphasense_driver_node/cam2/color/image/compressed",
             "/gt_box/alphasense_driver_node/cam2/color/camera_info",
@@ -228,65 +234,31 @@ if __name__ == "__main__":
             "/gt_box/alphasense_driver_node/cam5/color/camera_info",
             "*_nuc_alphasense_calib.bag",
         ),  # Color
-        "tf_static": ("tf_static", "*_tf_static.bag"),
-    }
-    process(alphasense_cfg, "alphasense")
-
-    hdr_cfg = {
-        "imu0": ("/gt_box/cpt7/offline_from_novatel_logs/imu", "*_cpt7_raw_imu.bag"),
-        "cam0": (
+        "cam5": (
             "/gt_box/hdr_left/image_raw/compressed",
             "/gt_box/hdr_left/camera_info",
             "*_jetson_hdr_left_calib.bag",
         ),
-        "cam1": (
+        "cam6": (
             "/gt_box/hdr_front/image_raw/compressed",
             "/gt_box/hdr_front/camera_info",
             "*_jetson_hdr_front_calib.bag",
         ),
-        "cam2": (
+        "cam7": (
             "/gt_box/hdr_right/image_raw/compressed",
             "/gt_box/hdr_right/camera_info",
             "*_jetson_hdr_right_calib.bag",
         ),
-        "tf_static": ("tf_static", "*_tf_static.bag"),
-    }
-    process(hdr_cfg, "hdr")
-
-    zed_cfg = {
-        "imu0": ("/gt_box/cpt7/offline_from_novatel_logs/imu", "*_cpt7_raw_imu.bag"),
-        "cam0": (
+        "cam8": (
             "/gt_box/zed2i/zed_node/left/image_rect_color/compressed",
             "/gt_box/zed2i/zed_node/left/camera_info",
             "*_jetson_zed2i_images_calib.bag",
         ),
-        "cam1": (
+        "cam9": (
             "/gt_box/zed2i/zed_node/right/image_rect_color/compressed",
             "/gt_box/zed2i/zed_node/right/camera_info",
             "*_jetson_zed2i_images_calib.bag",
         ),
         "tf_static": ("tf_static", "*_tf_static.bag"),
     }
-    process(zed_cfg, "zed")
-
-    alphasense_imus_cfg = {
-        "imu0": ("/gt_box/cpt7/offline_from_novatel_logs/imu", "*_cpt7_raw_imu.bag"),
-        "imu1": ("/gt_box/alphasense_driver_node/imu", "*_nuc_alphasense_calib.bag"),
-        "imu2": ("/gt_box/ap20/imu", "*_jetson_ap20_synced.bag"),
-        "imu3": ("/gt_box/zed2i/zed_node/imu_raw/data", "*_jetson_zed2i_prop.bag"),
-        "imu4": ("/gt_box/stim320/imu", "*_jetson_stim.bag"),
-        "imu5": ("/gt_box/adis16475_node/imu", "*_jetson_adis.bag"),
-        "imu6": ("/gt_box/livox/imu_si_compliant", "*_nuc_livox.bag"),
-        "cam0": (
-            "/gt_box/alphasense_driver_node/cam2/color/image/compressed",
-            "/gt_box/alphasense_driver_node/cam2/color/camera_info",
-            "*_nuc_alphasense_calib.bag",
-        ),  # Right
-        "cam1": (
-            "/gt_box/alphasense_driver_node/cam3/color/image/compressed",
-            "/gt_box/alphasense_driver_node/cam3/color/camera_info",
-            "*_nuc_alphasense_calib.bag",
-        ),  # Left
-        "tf_static": ("tf_static", "*_tf_static.bag"),
-    }
-    process(alphasense_imus_cfg, "alphasense_imu")
+    process(all_cameras_cfg, "all_cameras")
