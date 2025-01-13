233a1096381bddf0b3009d6feba0c796b8571912
JonasFrey96 <jonfrey@ethz.ch>
2025-01-13 15:51:56 +0100

    updated dlio

--- box_utils/box_auto/python/box_auto/scripts/application/dlio.py
+++ box_utils/box_auto/python/box_auto/scripts/application/dlio.py
@@ -3,7 +3,7 @@
 import os
 from time import sleep
 import shutil
-
+import argparse
 from box_auto.utils import (
     get_bag,
     upload_bag,
@@ -15,64 +15,17 @@ from box_auto.utils import (
     check_duplicate_timestamps,
 )
 
-PATTERNS = None
-OUTPUT_BAG_NAME = "dlio_replayed"
 
-# TODO make these env var. ?
-# Set IMU configuration
-USE_STIM320 = False
-USE_CPT7_IMU = True
-
-# Set LiDAR configuration
-USE_LIVOX = False
-USE_HESAI = True
-
-# Resolve IMU information
-imu_pattern = None
-if USE_STIM320:
-    imu_pattern = "*_jetson_stim.bag"
-    imu_topic = "/gt_box/stim320/imu"
-elif USE_CPT7_IMU:
-    imu_pattern = "*_cpt7_raw_imu.bag"
-    imu_topic = "/gt_box/cpt7/offline_from_novatel_logs/imu"
-
-if imu_pattern is None:
-    print(f"IMU pattern not set correctly: {imu_pattern}")
-    exit(-1)
-
-# Resolve LiDAR information prefer filtered pointclouds
-if USE_HESAI:
-
-    try:
-        get_bag("*_nuc_hesai_filtered.bag")
-        PATTERNS = [imu_pattern, "*_tf_static.bag", "*_nuc_hesai_filtered.bag"]
-        lidar_topic = "/gt_box/hesai/points_filtered"
-    except Exception:
-        PATTERNS = [imu_pattern, "*_tf_static.bag", "*_nuc_hesai_post_processed.bag"]
-        lidar_topic = "/gt_box/hesai/points"
-elif USE_LIVOX:
-    try:
-        get_bag("*_nuc_livox_filtered.bag")
-        PATTERNS = [imu_pattern, "*_tf_static.bag", "*_nuc_livox_filtered.bag"]
-        lidar_topic = "/gt_box/livox/lidar_filtered"
-    except:
-        PATTERNS = [imu_pattern, "*_tf_static.bag", "*_nuc_livox.bag"]
-        lidar_topic = "/gt_box/livox/lidar"
-
-else:
-    raise ValueError("Use Livox or Hesai")
-
-
-def launch_nodes():
+def launch_nodes(patterns, output_bag_name, lidar_topic, imu_topic, tag):
     os.environ["ROS_MASTER_URI"] = "http://localhost:11311"
     inputs = []
-    for pattern in PATTERNS:
+    for pattern in patterns:
         f = get_bag(pattern)
         inputs.append(f)
         timestamp = str(f).split("/")[-1]
         timestamp = timestamp.split("_")[0]
 
-    assert len(inputs) == len(PATTERNS), "Failure did not find all the bags required"
+    assert len(inputs) == len(patterns), "Failure did not find all the bags required"
 
     inputs = ",".join(inputs)
 
@@ -80,12 +33,11 @@ def launch_nodes():
 
     # Check if merged_rosbag_path already exists
     if os.path.exists(merged_rosbag_path):
-        print(f"Using existing merged rosbag at {merged_rosbag_path}")
-    else:
-        print(f"Merging bags into {merged_rosbag_path}")
-        os.system(
-            f"python3 {BOX_AUTO_SCRIPTS_DIR}/general/merge_bags.py --input={inputs} --output={merged_rosbag_path}"
-        )
+        print(f"Removing existing merged rosbag at {merged_rosbag_path}")
+        os.system(f"rm {merged_rosbag_path}")
+
+    print(f"Merging bags into {merged_rosbag_path}")
+    os.system(f"python3 {BOX_AUTO_SCRIPTS_DIR}/general/merge_bags.py --input={inputs} --output={merged_rosbag_path}")
 
     kill_roscore()
 
@@ -94,7 +46,7 @@ def launch_nodes():
     start_roscore()
     sleep(1)
     run_ros_command(
-        f"roslaunch direct_lidar_inertial_odometry dlio_replay.launch input_rosbag_path:={merged_rosbag_path}  output_rosbag_folder_path:={MISSION_DATA} output_rosbag_name:={OUTPUT_BAG_NAME} imu_topic:={imu_topic} pointcloud_topic:={lidar_topic}",
+        f"roslaunch direct_lidar_inertial_odometry dlio_replay.launch input_rosbag_path:={merged_rosbag_path}  output_rosbag_folder_path:={MISSION_DATA} output_rosbag_name:={output_bag_name} imu_topic:={imu_topic} pointcloud_topic:={lidar_topic}",
         background=True,
     )
     sleep(5)
@@ -104,14 +56,62 @@ def launch_nodes():
     print("Moving and uploading bag!")
     kill_roscore()
 
-    output_bag_path = os.path.join(MISSION_DATA, f"{timestamp}_dlio.bag")
-    shutil.move(f"{MISSION_DATA}/{OUTPUT_BAG_NAME}.bag", output_bag_path)
+    output_bag_path = os.path.join(MISSION_DATA, f"{timestamp}_{tag}dlio.bag")
+    shutil.move(f"{MISSION_DATA}/{output_bag_name}.bag", output_bag_path)
 
     check_duplicate_timestamps(output_bag_path, "/dlio/deskewed_point_cloud")
 
     upload_bag(output_bag_path)
 
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Configure LiDAR and IMU options.")
+    parser.add_argument(
+        "--lidar", choices=["hesai", "livox"], default="hesai", help="Select the LiDAR configuration (default: hesai)."
+    )
+    parser.add_argument(
+        "--imu", choices=["stim320", "cpt7"], default="cpt7", help="Select the IMU configuration (default: cpt7)."
+    )
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    launch_nodes()
+    args = parse_arguments()
+
+    # Resolve IMU information
+    if args.imu == "stim320":
+        imu_pattern = "*_jetson_stim.bag"
+        imu_topic = "/gt_box/stim320/imu"
+    elif args.imu == "cpt7":
+        imu_pattern = "*_cpt7_raw_imu.bag"
+        imu_topic = "/gt_box/cpt7/offline_from_novatel_logs/imu"
+    else:
+        print(f"IMU not found: {args.imu}")
+        exit(1)
+
+    # Resolve LiDAR information prefer filtered pointclouds
+    if args.lidar == "hesai":
+        try:
+            get_bag("*_nuc_hesai_filtered123.bag")
+            patterns = [imu_pattern, "*_tf_static.bag", "*_nuc_hesai_filtered.bag"]
+            lidar_topic = "/gt_box/hesai/points_filtered"
+        except Exception:
+            patterns = [imu_pattern, "*_tf_static.bag", "*_nuc_hesai_post_processed.bag"]
+            lidar_topic = "/gt_box/hesai/points"
+        tag = ""
+    elif args.lidar == "livox":
+        try:
+            get_bag("*_nuc_livox_filtered123.bag")
+            patterns = [imu_pattern, "*_tf_static.bag", "*_nuc_livox_filtered.bag"]
+            lidar_topic = "/gt_box/livox/lidar_filtered"
+        except:
+            patterns = [imu_pattern, "*_tf_static.bag", "*_nuc_livox.bag"]
+            lidar_topic = "/gt_box/livox/lidar"
+        tag = "livox_"
+    else:
+        print(f"LiDAR not supported: {args.lidar}")
+        exit(2)
+
+    output_bag_name = f"dlio_replayed_{args.lidar}_{args.imu}"
+    launch_nodes(patterns, output_bag_name, lidar_topic, imu_topic, tag)
     exit(0)
