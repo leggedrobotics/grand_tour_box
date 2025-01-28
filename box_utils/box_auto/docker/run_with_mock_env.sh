95bd7e8d10669b7b744eee85f1e8815f437a88d5
kappi <kappi.patterson@gmail.com>
2025-01-28 12:33:17 +0100

    updating comment

--- box_utils/box_auto/docker/run_with_mock_env.sh
+++ box_utils/box_auto/docker/run_with_mock_env.sh
@@ -5,7 +5,7 @@
 MISSION_UUID="f925eb38-92c3-4318-9ebb-712e43424589" # 2024-11-02-17-43-10
 ACTION_UUID="123-xyz" # Dummy action uuid
 PROJECT_UUID="226223c3-8593-4fb8-8e41-cc5d23e6ccb7" # Grand Tour Kappi Playground
-LOCAL_MISSION_DATA="/home/kappi/rsl/boxi/empty" # 2024-11-02-17-43-10
+LOCAL_MISSION_DATA="/home/kappi/rsl/boxi/empty" # Empty directory
 
 
 # Call the original script with the environment variables passed as --env
