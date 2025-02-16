621be9b0d0f5159a6933ac403bd372d4996384a1
JonasFrey96 <jonfrey@ethz.ch>
2025-02-16 18:02:32 +0100

    okvis automated run

--- box_utils/box_auto/python/box_auto/scripts/application/export_okvis.py
+++ box_utils/box_auto/python/box_auto/scripts/application/export_okvis.py
@@ -31,35 +31,111 @@ if __name__ == "__main__":
         },
     }
 
+    runs = {
+        f"run_folder_{k}": {
+            "data_folder": f"euroc_grand_tour/run_folder_{k}/",
+            "okvis_config": "okvis_config_imu0.yaml",
+        }
+        for k in range(7)
+    }
+
+    runs = {
+        "hdr1": {
+            "data_folder": "euroc_grand_tour/hdr1/",
+            "okvis_config": "okvis_config_imu0.yaml",
+        },
+        "hdr3": {
+            "data_folder": "euroc_grand_tour/hdr3/",
+            "okvis_config": "okvis_config_imu0.yaml",
+        },
+        "alphasense1": {
+            "data_folder": "euroc_grand_tour/alphasense1/",
+            "okvis_config": "okvis_config_imu0.yaml",
+        },
+        "alphasense2": {
+            "data_folder": "euroc_grand_tour/alphasense2/",
+            "okvis_config": "okvis_config_imu0.yaml",
+        },
+        "alphasense3": {
+            "data_folder": "euroc_grand_tour/alphasense3/",
+            "okvis_config": "okvis_config_imu0.yaml",
+        },
+        "zed2i1": {
+            "data_folder": "euroc_grand_tour/zed2i1/",
+            "okvis_config": "okvis_config_imu0.yaml",
+        },
+        "zed2i2": {
+            "data_folder": "euroc_grand_tour/zed2i2/",
+            "okvis_config": "okvis_config_imu0.yaml",
+        },
+    }
+
+    runs = {
+        "hdr1_adjust": {
+            "data_folder": "euroc_grand_tour/hdr1_adjust/",
+            "okvis_config": "okvis_config_imu0.yaml",
+        },
+        "hdr3_adjust": {
+            "data_folder": "euroc_grand_tour/hdr3_adjust/",
+            "okvis_config": "okvis_config_imu0.yaml",
+        },
+        "alphasense1_adjust": {
+            "data_folder": "euroc_grand_tour/alphasense1_adjust/",
+            "okvis_config": "okvis_config_imu0.yaml",
+        },
+        "alphasense2_adjust": {
+            "data_folder": "euroc_grand_tour/alphasense2_adjust/",
+            "okvis_config": "okvis_config_imu0.yaml",
+        },
+        "alphasense3_adjust": {
+            "data_folder": "euroc_grand_tour/alphasense3_adjust/",
+            "okvis_config": "okvis_config_imu0.yaml",
+        },
+        "zed2i1_adjust": {
+            "data_folder": "euroc_grand_tour/zed2i1_adjust/",
+            "okvis_config": "okvis_config_imu0.yaml",
+        },
+        "zed2i2_adjust": {
+            "data_folder": "euroc_grand_tour/zed2i2_adjust/",
+            "okvis_config": "okvis_config_imu0.yaml",
+        },
+    }
+    print(runs)
+
     for k, v in runs.items():
-        run_script = f"{BOX_AUTO_DIR}/docker/run.sh"
+        try:
+            run_script = f"{BOX_AUTO_DIR}/docker/run.sh"
 
-        relativ_euroc_data = v["data_folder"]
-        relativ_okvis_config = v["data_folder"] + v["okvis_config"]
-        command = f"/home/rsl/ros2_ws/build/okvis/okvis_app_synchronous  /tmp_disk/{relativ_okvis_config} /tmp_disk/{relativ_euroc_data}"
+            relativ_euroc_data = v["data_folder"]
+            relativ_okvis_config = v["data_folder"] + v["okvis_config"]
+            command = f"/home/rsl/ros2_ws/build/okvis/okvis_app_synchronous  /tmp_disk/{relativ_okvis_config} /tmp_disk/{relativ_euroc_data}"
 
-        cmd = run_script + f" --type=ros2 --mission-data={MISSION_DATA} --command='{command}'"
-        # Run okvis using ROS2 Docker container
-        # print(cmd)
-        # os.system(cmd)
-        print("We assume Okvis was run successfully")
+            cmd = run_script + f" --type=ros2 --mission-data={MISSION_DATA} --command='{command}'"
+            # Run okvis using ROS2 Docker container
+            # print(cmd)
+            # os.system(cmd)
+            print("We assume Okvis was run successfully")
 
-        metadata = {}
-        okvis_config_path = Path(MISSION_DATA) / (v["data_folder"] + v["okvis_config"])
-        with open(okvis_config_path, "r") as f:
-            metadata["okvis_config"] = f.read()
+            metadata = {}
+            okvis_config_path = Path(MISSION_DATA) / (v["data_folder"] + v["okvis_config"])
+            with open(okvis_config_path, "r") as f:
+                metadata["okvis_config"] = f.read()
 
-        euroc_config_path = Path(MISSION_DATA) / v["data_folder"] / "euroc_config.yaml"
-        with open(euroc_config_path, "r") as f:
-            metadata["euroc_config"] = f.read()
+            euroc_config_path = Path(MISSION_DATA) / v["data_folder"] / "euroc_config.yaml"
+            with open(euroc_config_path, "r") as f:
+                metadata["euroc_config"] = f.read()
 
-        metadata["date"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        metadata_string = yaml.dump(metadata, default_flow_style=False)
+            metadata["date"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            metadata_string = yaml.dump(metadata, default_flow_style=False)
 
-        # Convert trajectory into rosbag
-        livox_bag_path = get_bag("*_nuc_livox.bag")
-        input_csv = Path(MISSION_DATA) / v["data_folder"] / "okvis2-slam_trajectory.csv"
-        output_bag = livox_bag_path.replace("_nuc_livox", f"_okvis_{k}")
-        okviz_trajectory_to_bag(
-            input_csv, output_bag, "okvis_world", "cpt7_imu", metadata_string, "/gt_box/okvis/offline_metadata"
-        )
+            # Convert trajectory into rosbag
+            livox_bag_path = get_bag("*_nuc_livox.bag")
+            input_csv = (
+                Path(MISSION_DATA) / v["data_folder"] / "okvis2-slam-calib-final_trajectory.csv"
+            )  # "okvis2-slam_trajectory.csv"
+            output_bag = livox_bag_path.replace("_nuc_livox", f"_okvis_{k}")
+            okviz_trajectory_to_bag(
+                input_csv, output_bag, "okvis_world", "cpt7_imu", metadata_string, "/gt_box/okvis/offline_metadata"
+            )
+        except Exception as e:
+            print(e)
