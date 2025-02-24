6c607e8e4213780bfcaa04531a8dbf30f1249c5a
tutuna <tutuna@leggedrobotics.com>
2025-02-24 10:09:50 +0100

    change build to release and explicitly add debug mode export

--- box_utils/box_auto/docker/dependencies/opencv_gtsam.sh
+++ box_utils/box_auto/docker/dependencies/opencv_gtsam.sh
@@ -3,7 +3,7 @@ source /opt/ros/noetic/setup.bash
 cd /home/opencv_gtsam_ws
 catkin init
 catkin config --extend /opt/ros/noetic
-catkin config --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo
+catkin config --cmake-args -DCMAKE_BUILD_TYPE=Release
 
 
 cd /home/opencv_gtsam_ws/src; git clone https://github.com/catkin/catkin_simple.git --depth 1
