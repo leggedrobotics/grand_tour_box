1ed55534eafdb30dd5158af065965c73482c3db6
JonasFrey96 <jonfrey@ethz.ch>
2025-05-17 14:51:13 +0200

    modifeid dummy file

--- box_utils/box_converter/huggingface-dataset-conversion/rvl_compression/dummy.py
+++ box_utils/box_converter/huggingface-dataset-conversion/rvl_compression/dummy.py
@@ -4,10 +4,14 @@ import pathlib
 import inspect
 import numpy as np
 
-# Automatically add box_binding to sys.path
-sys.path.insert(0, "/app/rvl_compression")
 
+# Automatically add box_binding to sys.path
+sys.path.insert(
+    0,
+    "/catkin_ws/src/grand_tour_box/box_utils/box_converter/huggingface-dataset-conversion/rvl_compression/compressed_depth",
+)
 import compressed_depth
+
 
 print("package dir :", pathlib.Path(inspect.getfile(compressed_depth)).parent)
 print("has decode  :", hasattr(compressed_depth, "decode"))
