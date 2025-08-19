bda628053ae73179f18aa460792d370474bbdf6b
tutuna <tutuna@leggedrobotics.com>
2025-08-19 12:55:58 +0200

    Update etna setup

--- box_configuration/anymal/npc/alias.sh
+++ box_configuration/anymal/npc/alias.sh
@@ -3,7 +3,8 @@
 #alias tk='tmux kill-server'
 #alias tk_dev='tmux kill-session -a'
 
-alias l-npc='tmuxp load /home/integration/boxi_scripts_ws/src/grand_tour_box/box_launch/tmux/npc.yaml'
+# This is the alais to run on the NPC of the robot.
+alias l-npc='tmuxp load /home/${USER}/${GRANDTOUR_SCRIPT_WORKSPACE}/src/grand_tour_box/box_launch/tmux/npc.yaml'
 
 alias sync-clocks="sudo killall chronyd; sudo chronyd"
 
